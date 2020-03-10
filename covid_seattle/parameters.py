35cec10d98caf890fdf065cc867c2e0a4dc24c38
cliffckerr <questionably@gmail.com>
2020-03-10 15:40:33 -0700

    final version?

--- covid_seattle/parameters.py
+++ covid_seattle/parameters.py
@@ -24,7 +24,7 @@ def make_pars():
     pars['usepopdata'] = 0 # Whether or not to load actual population data
 
     # Epidemic parameters
-    pars['r_contact']      = 2.5/(8*20) # Updated to match Mike's distributions
+    pars['r_contact']      = 2.05/(8*20) # Updated to match Mike's distributions
     pars['contacts']       = 20 # Number of contacts per guest per day, estimated
     pars['incub']          = 4.0 # Using Mike's Snohomish number
     pars['incub_std']      = 1.0 # Standard deviation of the serial interval, estimated
