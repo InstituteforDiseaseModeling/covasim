46f5f0f633d2f2bb4e292a26804d85c5b8830c24
cliffckerr <questionably@gmail.com>
2020-03-11 17:54:10 -0700

    starting on brittany projections

--- covid_seattle/parameters.py
+++ covid_seattle/parameters.py
@@ -12,13 +12,17 @@ def make_pars():
     ''' Set parameters for the simulation '''
     pars = {}
 
+    wks_offset = 4 # Change this -- weeks of offset
+    offset = 7*wks_offset
+    factor = 2**(offset/6.2) # Doubling time
+
     # Simulation parameters
     pars['scale']      = 25 # Factor by which to scale results ## 100
 
     pars['n']          = int(0.4 * 3e6 // pars['scale']) # Number ultimately susceptible to CoV
-    pars['n_infected'] = 100 // pars['scale'] # Asked for 1000 in Seattle's population # 550
-    pars['day_0']      = 53 #datetime(2020, 2, 10) # Start day of the epidemic 3/5
-    pars['n_days']     = 45 # 75 #(datetime(2020, 4, 28)-pars['day_0']).days # How many days to simulate Apr/30 # 54
+    pars['n_infected'] = int(factor*100 // pars['scale']) # Asked for 1000 in Seattle's population # 550
+    pars['day_0']      = 53+offset #datetime(2020, 2, 10) # Start day of the epidemic 3/5
+    pars['n_days']     = 45-offset # 75 #(datetime(2020, 4, 28)-pars['day_0']).days # How many days to simulate Apr/30 # 54
     pars['seed']       = 1 # Random seed, if None, don't reset
     pars['verbose']    = 1 # Whether or not to display information during the run -- options are 0 (silent), 1 (default), 2 (everything)
     pars['usepopdata'] = 0 # Whether or not to load actual population data
