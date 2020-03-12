70dea3c4e69e9f62d316c46f2f875992791b0cdf
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-03-12 15:22:17 +0800

    Added plotly graphs

--- covid_webapp/covid_app.py
+++ covid_webapp/covid_app.py
@@ -5,10 +5,12 @@ Sciris app to run the web interface.
 # Imports
 import os
 import sys
-import mpld3
 import sciris as sc
 import scirisweb as sw
 import covid_seattle as cs
+import plotly_express as px
+import plotly.graph_objects as go
+import pylab as pl
 
 # Change to current folder and create the app
 app = sw.ScirisApp(__name__, name="Covid-ABM")
@@ -103,22 +105,75 @@ def plot_sim(sim_pars=None, epi_pars=None, verbose=True):
         print(err3)
         err += err3
 
-    # Plotting
-    try:
-        fig_args = {'figsize':(8,9)}
-        axis_args = {'left':0.15, 'bottom':0.1, 'right':0.9, 'top':0.95, 'wspace':0.2, 'hspace':0.25}
-        kwargs = dict(fig_args=fig_args, axis_args=axis_args, font_size=12, use_grid=False)
-        fig = sim.plot(**kwargs) # Plot the sim
-        mpld3.plugins.connect(fig, mpld3.plugins.MousePosition(fontsize=12, fmt='.4r')) # Add data cursor
-        graphjson = sw.mpld3ify(fig, jsonify=False)  # Convert to dict
-    except Exception as E:
-        err4 = f'Plotting failed!  ({str(E)})'
-        print(err4)
-        err += err4
-        graphjson ={}# {'err':err}
 
-    return {'graph':graphjson, 'err':err}  # Return the JSON representation of the Matplotlib figure
 
+    # fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+    # fig.update_layout(
+    #     title="Product cost",
+    # )
+    # output['graphs'].append({'json':fig.to_json(),'id':str(sc.uuid())})
+    # output['graphs'].append({'json':fig.to_json(),'id':str(sc.uuid())})
+
+    to_plot = sc.odict({
+        'Total counts': sc.odict({
+            'n_susceptible': 'Number susceptible',
+            'n_exposed': 'Number exposed',
+            'n_infectious': 'Number infectious',
+            'cum_diagnosed': 'Number diagnosed',
+            'cum_deaths': 'Number of deaths',
+        }),
+        'Daily counts': sc.odict({
+            'infections': 'New infections',
+            'tests': 'Number of tests',
+            'diagnoses': 'New diagnoses',
+            'deaths': 'New deaths',
+        })
+    })
+
+    if sim.data is not None:
+        data_mapping = {
+            'cum_diagnosed': pl.cumsum(sim.data['new_positives']),
+            'tests':         sim.data['new_tests'],
+            'diagnoses':     sim.data['new_positives'],
+            }
+    else:
+        data_mapping = {}
+
+    output = {}
+    output['err'] = err
+    output['graphs'] = []
+
+    for p,title,keylabels in to_plot.enumitems():
+        fig = go.Figure()
+        colors = sc.gridcolors(len(keylabels))
+        for i,key,label in keylabels.enumitems():
+            this_color = 'rgb(%d,%d,%d)' % (255*colors[i][0],255*colors[i][1],255*colors[i][2])
+            y = sim.results[key]
+            fig.add_trace(go.Scatter(x=sim.results['t'], y=y,mode='lines',name=label,line_color=this_color))
+            if key in data_mapping:
+                fig.add_trace(go.Scatter(x=sim.data['day'], y=data_mapping[key],mode='markers',name=label,fill_color=this_color))
+        fig.update_layout(title=title,
+                          xaxis_title='Days since index case',
+                          yaxis_title='Count')
+        output['graphs'].append({'json':fig.to_json(),'id':str(sc.uuid())})
+
+    return output
+
+    #
+    # # Plotting
+    # try:
+    #     fig_args = {'figsize':(8,9)}
+    #     axis_args = {'left':0.15, 'bottom':0.1, 'right':0.9, 'top':0.95, 'wspace':0.2, 'hspace':0.25}
+    #     kwargs = dict(fig_args=fig_args, axis_args=axis_args, font_size=12, use_grid=False)
+    #     fig = sim.plot(**kwargs) # Plot the sim
+    #     mpld3.plugins.connect(fig, mpld3.plugins.MousePosition(fontsize=12, fmt='.4r')) # Add data cursor
+    #     graphjson = sw.mpld3ify(fig, jsonify=False)  # Convert to dict
+    # except Exception as E:
+    #     err4 = f'Plotting failed!  ({str(E)})'
+    #     print(err4)
+    #     err += err4
+    #     graphjson ={}# {'err':err}
+    # return {'graph':graphjson, 'err':err}  # Return the JSON representation of the Matplotlib figure
 
 
 #%% Run the server
