f5311d8ec511b8cb581658994b39557b65439535
cliffckerr <questionably@gmail.com>
2020-03-14 13:55:07 -0700

    remove broken test

--- covasim/cova_cdc/model.py
+++ covasim/cova_cdc/model.py
@@ -11,7 +11,7 @@ import numpy as np # Needed for a few things not provided by pl
 import pylab as pl
 import sciris as sc
 import covasim.cova_base as cova
-from . import parameters as cov_pars
+from . import parameters as cova_pars
 
 
 # Specify all externally visible functions this file defines
@@ -75,9 +75,9 @@ class Sim(cova.Sim):
 
     def __init__(self, pars=None, datafile=None):
         if pars is None:
-            pars = cov_pars.make_pars()
+            pars = cova_pars.make_pars()
         super().__init__(pars) # Initialize and set the parameters as attributes
-        self.data = None # cov_pars.load_data(datafile)
+        self.data = None # cova_pars.load_data(datafile)
         self.set_seed(self['seed'])
         self.init_results()
         self.init_people()
@@ -121,7 +121,7 @@ class Sim(cova.Sim):
 
         self.people = {} # Dictionary for storing the people -- use plain dict since faster
         for p in range(int(self['n'])): # Loop over each person
-            age,sex = cov_pars.get_age_sex(use_data=self['usepopdata'])
+            age,sex = cova_pars.get_age_sex(use_data=self['usepopdata'])
             person = Person(self.pars, age=age, sex=sex) # Create the person
             self.people[person.uid] = person # Save them to the dictionary
 
--- covasim/cova_cruise/model.py
+++ covasim/cova_cruise/model.py
@@ -10,8 +10,8 @@ Version: 2020mar08
 import numpy as np # Needed for a few things not provided by pl
 import pylab as pl
 import sciris as sc
-import covid_abm as cova
-from . import parameters as cruise_pars
+import covasim.cova_base as cova
+from . import parameters as cova_pars
 
 
 # Specify all externally visible functions this file defines
@@ -60,9 +60,9 @@ class Sim(cova.Sim):
 
     def __init__(self, pars=None, datafile=None):
         if pars is None:
-            pars = cruise_pars.make_pars()
+            pars = cova_pars.make_pars()
         super().__init__(pars) # Initialize and set the parameters as attributes
-        self.data = cruise_pars.load_data(datafile)
+        self.data = cova_pars.load_data(datafile)
         self.set_seed(self['seed'])
         self.init_results()
         self.init_people()
@@ -100,7 +100,7 @@ class Sim(cova.Sim):
         guests = [0]*self['n_guests']
         crew   = [1]*self['n_crew']
         for is_crew in crew+guests: # Loop over each person
-            age,sex = cruise_pars.get_age_sex(is_crew)
+            age,sex = cova_pars.get_age_sex(is_crew)
             person = Person(self.pars, age=age, sex=sex, crew=is_crew) # Create the person
             self.people[person.uid] = person # Save them to the dictionary
 
