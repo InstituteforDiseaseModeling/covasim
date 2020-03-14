36385f1e8ddf52cc8c1c912904a222426284f869
cliffckerr <questionably@gmail.com>
2020-03-13 19:08:20 -0700

    tests pass

--- covid_abm/utils.py
+++ covid_abm/utils.py
@@ -31,16 +31,27 @@ def sample(dist=None, par1=None, par2=None, size=None):
 
     '''
 
+    choices = [
+        'uniform',
+        'normal',
+        'lognormal',
+        'normal_pos',
+        'normal_int',
+        'lognormal_int',
+        'neg_binomial'
+        ]
+
     # TODO: refactor using a dict mapping if possible
     if   dist == 'uniform':       samples = np.random.uniform(low=par1, high=par2, size=size)
     elif dist == 'normal':        samples = np.random.normal(loc=par1, scale=par2, size=size)
+    elif dist == 'normal_pos':    samples = np.abs(np.random.normal(loc=par1, scale=par2, size=size))
+    elif dist == 'normal_int':    samples = np.round(np.abs(np.random.normal(loc=par1, scale=par2, size=size)))
     elif dist == 'lognormal':     samples = np.random.lognormal(mean=par1, sigma=par2, size=size)
-    elif dist == 'normal_pos':    samples = np.maximum(0, np.random.normal(loc=par1, scale=par2, size=size))
-    elif dist == 'normal_int':    samples = np.maximum(0, np.round(np.random.normal(loc=par1, scale=par2, size=size)))
-    elif dist == 'lognormal_int': samples = np.maximum(0, np.round(np.random.lognormal(mean=par1, sigma=par2, size=size)))
+    elif dist == 'lognormal_int': samples = np.round(np.random.lognormal(mean=par1, sigma=par2, size=size))
     elif dist == 'neg_binomial':  samples = np.random.negative_binomial(n=par1, p=par2, size=size)
     else:
-        errormsg = f'The selected distribution "{dist}" is not implemented'
+        choicestr = '\n'.join(choices)
+        errormsg = f'The selected distribution "{dist}" is not implemented; choices are: {choicestr}'
         raise NotImplementedError(errormsg)
 
     return samples
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,54 +6,58 @@ Tests of the utilies for the model.
 import pytest
 import numpy as np
 import numba as nb
+import pylab as pl
 import sciris as sc
-import covid_abm
+import covid_abm as cova
+
+
+doplot = 1
 
 
 #%% Define the tests
 
 def test_rand():
-    
+
     default_seed = 1
-    
+
     @nb.njit
     def numba_rand():
         return np.random.rand()
-    
+
     # Check that two consecutive numbers don't match
-    covid_abm.set_seed(default_seed)
+    cova.set_seed(default_seed)
     a = np.random.rand()
     b = np.random.rand()
     v = numba_rand()
     w = numba_rand()
-    assert a != b 
+    assert a != b
     assert v != w
-    
+
     # Check that after resetting the seed, they do
-    covid_abm.set_seed(default_seed)
+    cova.set_seed(default_seed)
     c = np.random.rand()
     x = numba_rand()
-    assert a == c 
+    assert a == c
     assert v == x
-    
+
     # Check that resetting with no argument, they don't again
-    covid_abm.set_seed() 
+    cova.set_seed()
     d = np.random.rand()
     y = numba_rand()
-    covid_abm.set_seed()
+    cova.set_seed()
     e = np.random.rand()
     z = numba_rand()
-    assert d != e 
+    assert d != e
     assert y != z
-    
+
     return
 
 
 def test_poisson():
     sc.heading('Poisson distribution')
-    s1 = covid_abm.poisson_test(10, 10)
-    s2 = covid_abm.poisson_test(10, 15)
-    s3 = covid_abm.poisson_test(0, 100)
+    s1 = cova.poisson_test(10, 10)
+    s2 = cova.poisson_test(10, 15)
+    s3 = cova.poisson_test(0, 100)
     l1 = 1.0
     l2 = 0.05
     l3 = 1e-9
@@ -65,13 +69,61 @@ def test_poisson():
     print(f'f(10,15) {s2} > {l2}')
     print(f'f(0,100) {s3} < {l3}')
     return
-    
+
+
+def test_samples(doplot=False):
+    sc.heading('Samples distribution')
+
+    n = 10000
+    nbins = 40
+
+    # Warning, must match utils.py!
+    choices = [
+        'uniform',
+        'normal',
+        'lognormal',
+        'normal_pos',
+        'normal_int',
+        'lognormal_int',
+        'neg_binomial'
+        ]
+
+    if doplot:
+        pl.figure(figsize=(20,14))
+
+    # Run the samples
+    nchoices = len(choices)
+    nsqr = np.ceil(np.sqrt(nchoices))
+    results = {}
+    for c,choice in enumerate(choices):
+        if choice == 'neg_binomial':
+            par1 = 10
+            par2 = 0.5
+        elif choice in ['lognormal', 'lognormal_int']:
+            par1 = 1
+            par2 = 0.5
+        else:
+            par1 = 0
+            par2 = 5
+        results[choice] = cova.sample(dist=choice, par1=par1, par2=par2, size=n)
+
+        if doplot:
+            pl.subplot(nsqr, nsqr, c+1)
+            pl.hist(x=results[choice], bins=nbins)
+            pl.title(f'dist={choice}, par1={par1}, par2={par2}')
+
+    with pytest.raises(NotImplementedError):
+        cova.sample(dist='not_found')
+
+    return results
+
+
 
 def test_choose_people():
     sc.heading('Choose people')
-    x1 = covid_abm.choose_people(10, 5)
+    x1 = cova.choose_people(10, 5)
     with pytest.raises(Exception):
-        covid_abm.choose_people_weighted(10, 5) # Requesting mroe people than are available
+        cova.choose_people_weighted(10, 5) # Requesting mroe people than are available
     print(f'Uniform sample from 0-9: {x1}')
     return
 
@@ -82,17 +134,17 @@ def test_choose_people_weighted():
     samples = 5
     lin = np.arange(n)
     lin = lin/lin.sum()
-    x0 = covid_abm.choose_people_weighted([0.01]*n, samples)
-    x1 = covid_abm.choose_people_weighted(lin, samples)
-    x2 = covid_abm.choose_people_weighted([1, 0, 0, 0, 0], 1)
-    x3 = covid_abm.choose_people_weighted([0.5, 0.5, 0, 0, 0], 1)
+    x0 = cova.choose_people_weighted([0.01]*n, samples)
+    x1 = cova.choose_people_weighted(lin, samples)
+    x2 = cova.choose_people_weighted([1, 0, 0, 0, 0], 1)
+    x3 = cova.choose_people_weighted([0.5, 0.5, 0, 0, 0], 1)
     assert x2[0] == 0
     assert x3[0] in [0,1]
     assert len(x0) == len(x1) == samples
     with pytest.raises(Exception):
-        covid_abm.choose_people_weighted([0.5, 0, 0, 0, 0], 1) # Probabilities don't sum to 1
+        cova.choose_people_weighted([0.5, 0, 0, 0, 0], 1) # Probabilities don't sum to 1
     with pytest.raises(Exception):
-        covid_abm.choose_people_weighted([0.5, 0.5], 10) # Requesting mroe people than are available
+        cova.choose_people_weighted([0.5, 0.5], 10) # Requesting mroe people than are available
     print(f'Uniform sample 0-99: x0 = {x0}, mean {x0.mean()}')
     print(f'Weighted sample 0-99: x1 = {x1}, mean {x1.mean()}')
     print(f'All weight on 0: x2 = {x2}')
@@ -104,12 +156,13 @@ def test_choose_people_weighted():
 #%% Run as a script
 if __name__ == '__main__':
     sc.tic()
-    
+
     test_rand()
     test_poisson()
+    results = test_samples(doplot=doplot)
     test_choose_people()
     test_choose_people_weighted()
-    
+
     print('\n'*2)
     sc.toc()
     print('Done.')
