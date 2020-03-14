937d9e1818111655094ef63683be1305d389c175
cliffckerr <questionably@gmail.com>
2020-03-13 18:37:14 -0700

    convert to errors

--- covid_abm/parameters.py
+++ covid_abm/parameters.py
@@ -9,20 +9,17 @@ __all__ = ['make_pars', 'get_age_sex', 'load_data']
 
 def make_pars():
     ''' Set parameters for the simulation '''
-    pars = {}
-    return pars
+    raise NotImplementedError
 
 
 def get_age_sex():
     ''' Get the age and sex of each person '''
-    age, sex = 0, 0
-    return age, sex
+    raise NotImplementedError
 
 
 def load_data(filename=None):
     ''' Load data for comparing to the model output '''
-    data = None
-    return data
+    raise NotImplementedError
 
 
 
