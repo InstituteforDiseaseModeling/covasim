97ef27c12952e6faede1033814c034cef47c4450
cliffckerr <questionably@gmail.com>
2020-03-21 10:55:17 -0700

    tidy init

--- covasim/__init__.py
+++ covasim/__init__.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 
-
 #%% Print version and license information
 from .version import __version__, __versiondate__, __license__
-from . import requirements as _requirements
 print(__license__)
 
-
 #%% Check that requirements are met
+from . import requirements as _requirements
 _requirements.check_sciris()
 _requirements.check_scirisweb(die=False)
 _requirements.check_extra_libs()
-
 
 #%% Imports from here -- just the framework, basic functions, and base -- the "base" version
 from .framework import *
