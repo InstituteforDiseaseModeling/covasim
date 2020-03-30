74d2e4343e119984ff7a49e9d08ffb336ffeb814
Robyn Stuart <robyna.s@gmail.com>
2020-03-30 12:37:06 +1100

    add test file

---
+++ tests/test_beds.py
@@ -0,0 +1,79 @@
+'''
+Testing the effect of testing interventions in Covasim
+'''
+
+#%% Imports and settings
+import matplotlib
+matplotlib.use('TkAgg')
+import sciris as sc
+import covasim as cv
+
+do_plot   = 1
+do_show   = 0
+do_save   = 1
+debug     = 1
+keep_sims = 0
+fig_path  = 'results/testing_beds.png'
+
+def test_beds(do_plot=False, do_show=True, do_save=False, fig_path=None):
+    sc.heading('Test of bed capacity estimation')
+
+    sc.heading('Setting up...')
+
+    sc.tic()
+
+    n_runs = 3
+    verbose = 1
+
+    sim = cv.Sim()
+
+    # Define the scenarios
+    scenarios = {
+        'baseline': {
+          'name': 'No bed constraints',
+          'pars': {
+              'n_infected': 100
+          }
+        },
+        'bedconstraint': {
+            'name': 'Only 10 beds available',
+            'pars': {
+                'n_infected': 100,
+                'n_beds': 10,
+            }
+        },
+        'bedconstraint2': {
+            'name': 'Only 1 bed available, people are 10x more likely to die if not hospitalized',
+            'pars': {
+                'n_infected': 100,
+                'n_beds': 1,
+                'OR_no_treat': 10.,
+            }
+        },
+    }
+
+    metapars = {'n_runs': n_runs}
+
+    scens = cv.Scenarios(sim=sim, metapars=metapars, scenarios=scenarios)
+    scens.run(verbose=verbose, debug=debug, keep_sims=keep_sims)
+
+    if do_plot:
+        to_plot = sc.odict({
+            'cum_deaths': 'Cumulative deaths',
+            'bed_capacity': 'Percentage bed capacity',
+        })
+        scens.plot(to_plot=to_plot, do_save=do_save, do_show=do_show, fig_path=fig_path)
+
+    return scens
+
+
+#%% Run as a script
+if __name__ == '__main__':
+    sc.tic()
+
+    sim = test_beds(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_path)
+
+    sc.toc()
+
+
+print('Done.')
