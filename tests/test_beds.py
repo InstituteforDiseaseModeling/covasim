6cfc5b0f2e042f98d856878c9aba4288d90b9f5a
cliffckerr <questionably@gmail.com>
2020-03-30 01:04:39 -0700

    small updates

--- tests/test_beds.py
+++ tests/test_beds.py
@@ -7,8 +7,8 @@ import sciris as sc
 import covasim as cv
 
 do_plot   = 1
-do_show   = 0
-do_save   = 1
+do_show   = 1
+do_save   = 0
 debug     = 1
 keep_sims = 0
 fig_path  = 'results/testing_beds.png'
