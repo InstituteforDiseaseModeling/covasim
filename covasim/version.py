6b0a0401deb0d0741dba9beabab934a6b971b67b
Cory Gwin <gwincr11@github.com>
2020-04-03 21:02:23 -0400

    fix string formating, move dataset formater tests

--- covasim/version.py
+++ covasim/version.py
@@ -4,4 +4,4 @@ __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.22.0'
 __versiondate__ = '2020-03-31'
-__license__ = f'Covasim {__version__} ({__versiondate__}) — © 2020 by IDM'
+__license__ = 'Covasim {0} ({1}) — © 2020 by IDM'.format(__version__, __versiondate__)
