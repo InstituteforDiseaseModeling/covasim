9ea012c8af8daff1770817de58767a773eb3158b
cliffckerr <questionably@gmail.com>
2020-04-05 19:14:56 -0700

    split tests

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -67,73 +67,6 @@ def test_sim(do_plot=False, do_save=False, do_show=False): # If being run via py
     return sim
 
 
-def test_singlerun():
-    sc.heading('Single run test')
-
-    iterpars = {'beta': 0.035,
-                }
-
-    sim = cv.Sim()
-    sim['n_days'] = 20
-    sim['n'] = 1000
-    sim = cv.single_run(sim=sim, **iterpars)
-
-    return sim
-
-
-def test_combine(do_plot=False): # If being run via pytest, turn off
-    sc.heading('Combine results test')
-
-    n_runs = 3
-    n = 1000
-    n_infected = 10
-
-    print('Running first sim...')
-    sim = cv.Sim({'n':n, 'n_infected':n_infected})
-    sim = cv.multi_run(sim=sim, n_runs=n_runs, combine=True)
-    assert len(sim.people) == n*n_runs
-
-    print('Running second sim, results should be similar but not identical (stochastic differences)...')
-    sim2 = cv.Sim({'n':n*n_runs, 'n_infected':n_infected*n_runs})
-    sim2.run()
-
-    if do_plot:
-        sim.plot()
-        sim2.plot()
-
-    return sim
-
-
-def test_multirun(do_plot=False): # If being run via pytest, turn off
-    sc.heading('Multirun test')
-
-    # Note: this runs 3 simulations, not 3x3!
-    iterpars = {'beta': [0.015, 0.025, 0.035],
-                'cont_factor': [0.1, 0.5, 0.9],
-                }
-
-    sim = cv.Sim()
-    sim['n_days'] = 60
-    sim['n'] = 1000
-    sims = cv.multi_run(sim=sim, iterpars=iterpars)
-
-    if do_plot:
-        for sim in sims:
-            sim.plot()
-
-    return sims
-
-
-def test_scenarios(do_plot=False):
-    sc.heading('Scenarios test')
-    basepars = {'n':1000}
-    scens = cv.Scenarios(basepars=basepars)
-    scens.run()
-    if do_plot:
-        scens.plot()
-    return scens
-
-
 def test_fileio():
     sc.heading('Test file saving')
 
@@ -218,10 +151,6 @@ if __name__ == '__main__':
     pars  = test_parsobj()
     sim0  = test_microsim()
     sim1  = test_sim(do_plot=do_plot, do_save=do_save, do_show=do_show)
-    sim2  = test_singlerun()
-    sim3  = test_combine(do_plot=do_plot)
-    sims  = test_multirun(do_plot=do_plot)
-    scens = test_scenarios(do_plot=do_plot)
     json  = test_fileio()
     sim4  = test_start_stop()
     sim5  = test_sim_data(do_plot=do_plot, do_show=do_show)
