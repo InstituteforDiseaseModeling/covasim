5bbedca45476c68c162cc7245637d8f073f852de
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-04-08 07:41:15 +0800

    Add example implementation of clustered random network

--- covasim/population.py
+++ covasim/population.py
@@ -2,7 +2,7 @@ import numpy as np  # Needed for a few things not provided by pl
 import sciris as sc
 from . import utils as cvu
 from . import person as cvper
-
+from collections import defaultdict
 
 class Population(sc.prettyobj):
     """
@@ -112,6 +112,112 @@ class Population(sc.prettyobj):
             self.contact_layers['Community'] = RandomContactLayer(name='Community', max_n=n_people, n=n_random_contacts)
 
         return self
+
+
+    @classmethod
+    def random_clustered(cls, pars, n_people:int, layer_cluster_sizes: dict, n_community_contacts: int):
+        """
+        Random network with symmetric clusters
+
+        Args:
+            pars:
+            n_people:
+            layer_cluster_sizes: Dict like {'H':4, 'W':20} specifying both layers and cluster size. Cannot include 'C'
+            n_community_contacts: Number of community contacts. Will automatically create a 'C' `RandomContactLayer` for this if value is > 0
+
+        Returns: A `Population` instance
+
+        """
+
+        self = cls()
+
+        if 'C' in layer_cluster_sizes and n_community_contacts > 0:
+            raise Exception('Layer name "C" is automatically created for community contacts if requested, cannot be used for a clustered layer')
+
+        # Load age data based on 2018 Seattle demographics
+        age_data = np.array([
+            [0, 4, 0.0605],
+            [5, 9, 0.0607],
+            [10, 14, 0.0566],
+            [15, 19, 0.0557],
+            [20, 24, 0.0612],
+            [25, 29, 0.0843],
+            [30, 34, 0.0848],
+            [35, 39, 0.0764],
+            [40, 44, 0.0697],
+            [45, 49, 0.0701],
+            [50, 54, 0.0681],
+            [55, 59, 0.0653],
+            [60, 64, 0.0591],
+            [65, 69, 0.0453],
+            [70, 74, 0.0312],
+            [75, 79, 0.02016],  # Calculated based on 0.0504 total for >=75
+            [80, 84, 0.01344],
+            [85, 89, 0.01008],
+            [90, 99, 0.00672],
+        ])
+
+        n_people = int(n_people)
+        n_community_contacts = int(n_community_contacts)
+
+        # Handle sex and UID
+        uids = sc.uuid(which='ascii', n=n_people, length=10, tostring=True)
+        sexes = cvu.rbt(0.5, n_people)
+
+        # Handle ages
+        age_data_min = age_data[:, 0]
+        age_data_max = age_data[:, 1] + 1  # Since actually e.g. 69.999
+        age_data_range = age_data_max - age_data_min
+        age_data_prob = age_data[:, 2]
+        age_data_prob /= age_data_prob.sum()  # Ensure it sums to 1
+        age_bins = cvu.mt(age_data_prob, n_people)  # Choose age bins
+        ages = age_data_min[age_bins] + age_data_range[age_bins] * np.random.random(n_people)  # Uniformly distribute within this age bin
+
+        # Instantiate people
+        self.people = {uid: cvper.Person(pars=pars, uid=uid, age=age, sex=sex) for uid, age, sex in zip(uids, ages, sexes)}
+        self._uids = {i: x.uid for i, x in enumerate(self.people.values())}
+
+        # Make contacts
+        self.contact_layers = {}
+
+        # Make static contact matrix
+        for layer_name, cluster_size in layer_cluster_sizes.items():
+            # Make clusters - each person belongs to one cluster
+            n_remaining = n_people
+            contacts = defaultdict(set) # Use defaultdict of sets for convenience while initializing. Could probably change this as part of performance optimization
+
+            while n_remaining > 0:
+
+                # Get the size of this cluster
+                this_cluster =  cvu.pt(cluster_size)  # Sample the cluster size
+                if this_cluster > n_remaining:
+                    this_cluster = n_remaining
+
+                # Indices of people in this cluster
+                cluster_indices = (n_people-n_remaining)+np.arange(this_cluster)
+
+                # Add symmetric pairwise contacts in each cluster. Can probably optimize this
+                for i in cluster_indices:
+                    for j in cluster_indices:
+                        if j <= i:
+                            pass
+                        else:
+                            contacts[self._uids[i]].add(j)
+                            contacts[self._uids[j]].add(i)
+
+                n_remaining -= this_cluster
+
+            contacts = {x:list(y) for x,y in contacts.items()} # Convert to a fixed dict of lists, rather than a defaultdict of sets
+            self.contact_layers[layer_name] = StaticContactLayer(name=layer_name, contacts=contacts)
+
+        # Make random contacts
+        if n_community_contacts > 0:
+            self.contact_layers['C'] = RandomContactLayer(name='C', max_n=n_people, n=int(n_community_contacts))
+
+        return self
+
+
+
 
     @classmethod
     def synthpops(cls, pars, n_people=5000, n_random_contacts: int = 20, betas=None):
