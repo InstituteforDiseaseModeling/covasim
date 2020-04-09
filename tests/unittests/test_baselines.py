3057baeaf4aa20ede2306e0799189739ecf62e26
cliffckerr <questionably@gmail.com>
2020-04-09 04:02:13 -0700

    update baseline

--- tests/unittests/test_baselines.py
+++ tests/unittests/test_baselines.py
@@ -22,29 +22,39 @@ def test_baseline():
     new = sim.summary
 
     # Compare keys
+    errormsg = ''
     old_keys = set(old.keys())
     new_keys = set(new.keys())
     if old_keys != new_keys:
-        errormsg = f"Keys don't match; old: {old_keys}; new: {new_keys}"
-        raise KeyError(errormsg)
+        errormsg = f"Keys don't match!\n"
+        missing = old_keys - new_keys
+        extra = new_keys - old_keys
+        if missing:
+            errormsg += f'  Missing old keys: {missing}\n'
+        if extra:
+            errormsg += f'  Extra new keys: {extra}\n'
 
     mismatches = {}
-    for key in old.keys():
-        old_val = old[key]
-        new_val = new[key]
+    for key in old_keys.union(new_keys):
+        old_val = old[key] if key in old else 'not present'
+        new_val = new[key] if key in new else 'not present'
         if old_val != new_val:
             mismatches[key] = {'old': old_val, 'new': new_val}
 
     if len(mismatches):
-        errormsg = '\nThe following values have changed between old and new!\n'
-        errormsg += 'Please rerun "update_baseline" if this is intentional.\n'
-        errormsg += 'Mismatches:\n'
-        space = ' '*15
+        errormsg += '\nMismatches:\n'
+        space = ' '*17
         for mkey,mval in mismatches.items():
-            errormsg += f'{mkey}:\n'
+            errormsg += f'  {mkey}:\n'
             errormsg += f'{space}old = {mval["old"]}\n'
             errormsg += f'{space}new = {mval["new"]}\n'
-        raise ValueError(errormsg)
+
+    # Raise an error if mismatches were found
+    if errormsg:
+        prefix = '\nThe following values have changed between the previous baseline and now!\n'
+        prefix += 'If this is intentional, please rerun "update_baseline" and commit.\n\n'
+        err = prefix + errormsg
+        raise ValueError(err)
     else:
         print('Baseline matches')
 
