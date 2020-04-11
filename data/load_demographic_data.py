46aa633237937a945ead8be3b4febeb87151cba3
cliffckerr <questionably@gmail.com>
2020-04-11 00:26:51 -0700

    combined scripts

--- data/load_demographic_data.py
+++ data/load_demographic_data.py
@@ -111,3 +111,28 @@ def available_countries():
     # UI https://github.com/InstituteforDiseaseModeling/covasim/blob/adc95ec589cbf0b06cbca8d8ba2b2a536a549ee7/covasim/webapp/cova_app.py#L38-L69
     # May need to think about how to create this...
     return NeherLabPop().countries()
+
+
+##
+# Update Sources
+#
+# This file will update all the population sources by
+# Pulling in different sources, translating them to the prefered
+# format and storing them locally.
+#
+# Add new loaders by addng the classes to sources and creating a
+# new dataloader that inherits from the Dataloader class.
+#
+# Run with python covasim/datasets/update_sources.py
+#
+from covasim.datasets.data_loader import load_country_pop
+from covasim.datasets.data_loader import NeherLabPop
+
+def load_sources():
+    print("Updating data sources")
+    source = NeherLabPop()
+    source.update_data()
+
+if __name__ == '__main__':
+    load_sources()
+    print(load_country_pop("Albania"))
