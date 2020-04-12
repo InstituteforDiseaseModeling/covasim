406a95d48ad52db794d858154605f33c6c02b4af
cliffckerr <questionably@gmail.com>
2020-04-11 17:20:15 -0700

    performance test for multiplication

--- tests/misc/numbatests.py
+++ tests/misc/numbatests.py
@@ -3,56 +3,59 @@ Speed and implementation tests
 '''
 
 import numpy as np
+import pylab as pl
 import sciris as sc
 import numba as nb
 
 class NumbaTests(sc.prettyobj):
-    def __init__(self, n=100e3, npts=100, keys=None):
+    def __init__(self, n=400e3, npts=1000, keys=None):
         if keys is None:
             keys = ['a','b']
         self.n = int(n)
         self.npts = npts
         self.keys = keys
         self.states = {key:np.random.random(self.n) for key in self.keys}
-        self.attr_a = np.random.random(self.n)
-        self.attr_b = np.random.random(self.n)
         self.results = np.zeros(self.npts)
 
     @property
-    def prop_a(self):
+    def a(self):
         return self.states['a']
 
     @property
-    def prop_b(self):
+    def b(self):
         return self.states['b']
 
-    def prop_next(self, t):
-        self.results[t] += (self.prop_a * self.prop_b).sum()
+    def next(self, t):
+        self.results[t] += (self.a * self.b).sum()
         return
 
-    def attr_next(self, t):
-        self.results[t] += (self.attr_a * self.attr_b).sum()
-        return
-
-    def prop_run(self):
+    def run(self):
         for t in range(self.npts):
-            self.prop_next(t)
-        return
-
-    def attr_run(self):
-        for t in range(self.npts):
-            self.attr_next(t)
+            self.next(t)
         return
 
 
 if __name__ == '__main__':
 
-    nt = NumbaTests()
+    ns = (1+np.arange(15))*100e3
 
-    sc.tic()
-    nt.prop_run()
-    sc.toc(label='prop')
+    ts = []
+    for n in ns:
+        nt = NumbaTests(n=n)
+        sc.tic()
+        nt.run()
+        t = sc.toc(output=True)
+        ts.append(t)
+        print(n, t)
 
-    sc.tic()
-    nt.attr_run()
-    sc.toc(label='attr')
+    pl.subplot(2,1,1)
+    pl.scatter(ns/1e6, ts)
+    sc.setylim()
+    pl.xlabel('Number of points (millions)')
+    pl.ylabel('Calculation time')
+
+    pl.subplot(2,1,2)
+    pl.scatter(ns/1e6, np.array(ts)/ns*1e6)
+    sc.setylim()
+    pl.xlabel('Number of points (millions)')
+    pl.ylabel('Calculation time per point (μs)')
