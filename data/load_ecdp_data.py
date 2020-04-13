7ebf69f2f017a31f5e550b6ea978cae6ba81c091
Will Fitzgerald <willf@github.com>
2020-04-13 12:48:55 -0400

    remove sciris dependency

--- data/load_ecdp_data.py
+++ data/load_ecdp_data.py
@@ -3,16 +3,11 @@ import pandas as pd
 import os
 import toml
 
-import sciris as sc
-subfolder = "epi_data"
-here = sc.thisdir(__file__)
-data_home = os.path.join(here, subfolder)
-
 parameter_definitions = f"""
 
 title = "European Centre for Disease Prevention and Control Covid-19 Data Scraper"
-# load_path = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
-load_path = "/Users/willf/github/covasim/data/epi_data/epi_test.csv"
+load_path = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+# load_path = "/Users/willf/github/covasim/data/epi_data/epi_test.csv"
 
 output_folder = "epi_data"
 output_filename = "ecdp_data.csv"
