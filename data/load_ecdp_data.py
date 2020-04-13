c8a6e5b071f0061dd40fcb211bf4d90d9db9ba24
Will Fitzgerald <willf@github.com>
2020-04-13 12:41:10 -0400

    convert ecdc scraper

--- data/load_ecdp_data.py
+++ data/load_ecdp_data.py
@@ -1,52 +1,45 @@
-'''
-This script creates a single file containing all the 
-European Centre for Disease Prevention and Control data.
-'''
-
-import os
-import sys
-import logging
+from data.scraper import Scraper
 import pandas as pd
+import os
+import toml
+
 import sciris as sc
-
-subfolder = 'epi_data'
-outputfile = 'ecdp_data.csv'
-
-log = logging.getLogger("ECDP Data Loader")
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-
-# Read in the European Centre for Disease Prevention and Control Data into a dataframe.
-log.info("Loading European Centre for Disease Prevention and Control timeseries data from opendata.ecdc.europa.eu")
-df = pd.read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv")
-log.info(f"Loaded {len(df)} records; now transforming data")
-
-# Use internal data to create dates; only keep `date` column
-df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
-df.drop(['dateRep', 'day', 'month', 'year'], inplace=True, axis=1)
-
-# Just to be safe, let's sort by name and date. Probably not 
-# necessary but better safe than sorry!.
-df = df.sort_values(['countriesAndTerritories', 'date'])
-
-# Each data set has a unique name. Let's create groups.
-g = df.groupby('countriesAndTerritories')
-
-# The parameter 'day' is the number of days since the first 
-# day of data collection for the group.
-df['day'] = g['date'].transform(lambda x: (x - min(x))).apply(lambda x: x.days)
-
-# We'll 'rename' some of the columns to be consistent
-# with the parameters file.
-df['new_positives'] = df.cases
-df['new_death'] = df.deaths
-df['population'] = df.popData2018
-df.drop(['cases', 'deaths', 'popData2018'], inplace=True, axis=1)
-
-
-# And save it to the data directory.
+subfolder = "epi_data"
 here = sc.thisdir(__file__)
 data_home = os.path.join(here, subfolder)
-filepath = sc.makefilepath(filename=outputfile, folder=data_home)
-log.info(f"Saving to {filepath}")
-df.to_csv(filepath)
-log.info(f"Script complete")
+
+parameter_definitions = f"""
+
+title = "European Centre for Disease Prevention and Control Covid-19 Data Scraper"
+# load_path = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+load_path = "/Users/willf/github/covasim/data/epi_data/epi_test.csv"
+
+output_folder = "epi_data"
+output_filename = "ecdp_data.csv"
+
+renames.countriesAndTerritories = "key"
+renames.cases = "new_positives"
+renames.deaths = "new_death"
+renames.popData2018 = "population"
+
+fields_to_drop = [
+    "dateRep", 
+    "month",
+    "year",  
+    "geoId", 
+    "countryterritoryCode"
+    ]
+
+"""
+
+
+
+class ECDPScraper(Scraper):
+    def create_date(self):
+        self.df["date"] = pd.to_datetime(self.df[["year", "month", "day"]])
+
+
+parameters = toml.loads(parameter_definitions)
+ECDPScraper(parameters)
+
+
