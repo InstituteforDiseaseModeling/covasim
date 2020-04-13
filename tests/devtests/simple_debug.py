fa0f2a044e21df5392cdb1c7172e710ed0152e17
cliffckerr <questionably@gmail.com>
2020-04-12 20:18:29 -0700

    with 64: 2.98 s run, 8.53 s total

--- tests/devtests/simple_debug.py
+++ tests/devtests/simple_debug.py
@@ -9,6 +9,6 @@ if debug:
     import warnings
     warnings.simplefilter("error")
 
-sim = cv.Sim(pop_size=20000, pop_infected=50, n_days=90)
+sim = cv.Sim(pop_size=100000, pop_infected=50, n_days=180)
 sim.run()
 sim.plot()
