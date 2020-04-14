544f8b5cb31a1ef2978599fc0316c60889950b3b
Will Fitzgerald <willf@github.com>
2020-04-14 11:40:12 -0400

    update parameter format

--- data/load_corona_data_scraper_data.py
+++ data/load_corona_data_scraper_data.py
@@ -4,54 +4,49 @@ data from the Corona Data Scraper.
 '''
 
 from data.scraper import Scraper
-import toml
-
-parameter_definitions = f"""
-
-title = "Corona Data Scraper Project Scraper"
-load_path = "https://coronadatascraper.com/timeseries.csv"
-# load_path = "/Users/willf/github/covasim/data/epi_data/cds_input.csv"
-
-output_folder = "epi_data"
-output_filename = "corona_data_scraper.csv"
-
-renames.name = "key"
-renames.cases = "cum_positives"
-renames.deaths = "cum_death"
-renames.tested = "cum_tests"
-renames.hospitalized = "cum_hospitalized"
-renames.discharged = "cum_discharged"
-renames.recovered = "cum_recovered"
-renames.active = "cum_active"
-
-cumulative_fields.cum_positives = "positives"
-cumulative_fields.cum_death = "death"
-cumulative_fields.cum_tests = "tests"
-cumulative_fields.cum_hospitalized = "hospitalized"
-cumulative_fields.cum_discharged = "discharged"
-cumulative_fields.cum_recovered = "recovered"
-cumulative_fields.cum_active = "active"
-
-
-fields_to_drop = [
-    "growthFactor", 
-    "city",
-    "county",  
-    "state", 
-    "country",
-    "lat",
-    "long",
-    "url",
-    "tz",
-    "level"
-    ]
-
-"""
-
 
 class CoronaDataScraperScraper(Scraper):
     pass
 
+parameters = dict()
+parameters['title'] = 'Corona Data Scraper Project Scraper'
+parameters['load_path'] = 'https://coronadatascraper.com/timeseries.csv'
+parameters['load_path'] = '/Users/willf/github/covasim/data/epi_data/cds_input.csv'
 
-parameters = toml.loads(parameter_definitions)
+parameters['output_folder'] = 'epi_data'
+parameters['output_filename'] = 'corona_data_scraper.csv'
+
+parameters['renames'] = dict()
+parameters['renames']['name'] = 'key'
+parameters['renames']['cases'] = 'cum_positives'
+parameters['renames']['deaths'] = 'cum_death'
+parameters['renames']['tested'] = 'cum_tests'
+parameters['renames']['hospitalized'] = 'cum_hospitalized'
+parameters['renames']['discharged'] = 'cum_discharged'
+parameters['renames']['recovered'] = 'cum_recovered'
+parameters['renames']['active'] = 'cum_active'
+
+parameters['cumulative_fields'] = dict()
+parameters['cumulative_fields']['cum_positives'] = 'positives'
+parameters['cumulative_fields']['cum_death'] = 'death'
+parameters['cumulative_fields']['cum_tests'] = 'tests'
+parameters['cumulative_fields']['cum_hospitalized'] = 'hospitalized'
+parameters['cumulative_fields']['cum_discharged'] = 'discharged'
+parameters['cumulative_fields']['cum_recovered'] = 'recovered'
+parameters['cumulative_fields']['cum_active'] = 'active'
+
+
+parameters['fields_to_drop'] = [
+    'growthFactor',
+    'city',
+    'county',
+    'state',
+    'country',
+    'lat',
+    'long',
+    'url',
+    'tz',
+    'level'
+]
+
 CoronaDataScraperScraper(parameters).scrape()
