4325cf10d0afc21b3bd8f832fa291bd3721eb499
cliffckerr <questionably@gmail.com>
2020-04-16 20:40:34 -0700

    refactored test

--- tests/devtests/dev_test_basic_test_prob.py
+++ tests/devtests/dev_test_basic_test_prob.py
@@ -1,25 +1,25 @@
-import json
-from covasim import Sim, parameters, test_prob, contact_tracing, sequence
+import covasim as cv
 
-my_parameters = parameters.make_pars()
+debug = False
+
+my_parameters = cv.parameters.make_pars()
 
 tweaking = {
     'n_days': 60
 }
 
 test_prob_start_day = 35
-prob_test_perfect_symptomatic = test_prob(symp_prob=1.0, symp_quar_prob=1.0, asymp_quar_prob=1.0,
+prob_test_perfect_symptomatic = cv.test_prob(symp_prob=1.0, symp_quar_prob=1.0, asymp_quar_prob=1.0,
                                           test_sensitivity=1.0, test_delay=1, start_day=test_prob_start_day)
 
 my_parameters['interventions'] = prob_test_perfect_symptomatic
 
-test_stuff_simulation = Sim(my_parameters)
+test_stuff_simulation = cv.Sim(my_parameters)
 test_stuff_simulation.run()
 
 test_stuff_results = test_stuff_simulation.to_json(tostring=False)
-with open("DEBUG_test_stuff_simulation.json","w") as outfile:
-    json.dump(test_stuff_results, outfile, indent=4, sort_keys=True)
-    pass
+if debug:
+    test_stuff_simulation.to_json(filename="DEBUG_test_stuff_simulation.json")
 
 assert test_stuff_results["results"]["n_symptomatic"][test_prob_start_day] > 0  # If there are symptomatics
 assert sum(test_stuff_results["results"]["new_tests"][test_prob_start_day:test_prob_start_day + 5]) > 0  # then there should be tests
