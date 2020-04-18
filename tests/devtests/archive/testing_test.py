15fd68564225629ac725c0062b05ceb034707312
cliffckerr <questionably@gmail.com>
2020-04-17 19:34:10 -0700

    copied data

--- tests/devtests/archive/testing_test.py
+++ tests/devtests/archive/testing_test.py
@@ -5,17 +5,24 @@ do_plot = 1
 
 datafile = sc.thisdir(__file__, '../../example_data.csv')
 
-testnum = cv.test_num(daily_tests=[1000]*60)
+
 
 pars = sc.objdict(
-    # beta = 0.0
     diag_factor = 1.0,
-    interventions=testnum,
     n_days=30,
     pop_infected=100
     )
 
 
 sim = cv.Sim(pars, datafile=datafile)
+
+
+choice = 2
+if choice == 1:
+    testnum = cv.test_num(daily_tests=[1000]*60)
+elif choice == 2:
+    testnum = cv.test_num(daily_tests=sim.data['new_tests'])
+
+sim.update_pars(interventions=testnum)
 
 sim.run(do_plot=do_plot)
