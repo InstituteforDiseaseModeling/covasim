0ce3a6a34193719833e197cb1988ddc3221af5f9
cliffckerr <questionably@gmail.com>
2020-04-17 20:09:40 -0700

    finished testing

--- tests/devtests/archive/testing_test2.py
+++ tests/devtests/archive/testing_test2.py
@@ -16,19 +16,21 @@ pars = sc.objdict(
 
 sim = cv.Sim(pars, datafile=datafile)
 
-case = 4
-if case == 0: # works, no diagnoses
-    testprob = cv.test_prob(symp_prob=0, asymp_prob=0, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0)
-elif case == 1: # works, most diagnosed, slight mismatch due to new infections that day
-    testprob = cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0)
-elif case == 2: # works, no diagnoses
-    testprob = cv.test_prob(symp_prob=1, asymp_prob=1,test_sensitivity=0.0, loss_prob=0.0, test_delay=0, start_day=0)
-elif case == 3: # works, ~80% diagnosed
-    testprob = cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=0.5, loss_prob=0.0, test_delay=0, start_day=0)
-elif case == 4: # works, ~80% diagnosed
-    testprob = cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.5, test_delay=0, start_day=0)
+case = 2
+testprobdict = {
+    0: cv.test_prob(symp_prob=0, asymp_prob=0, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0), # works, no diagnoses
+    1: cv.test_prob(symp_prob=1, asymp_prob=0, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0), # works, ~half diagnosed
+    2: cv.test_prob(symp_prob=0, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0), # works, most diagnosed (during asymptomatic period)
+    3: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0), # works, most diagnosed, slight mismatch due to new infections that day
+    4: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=0.0, loss_prob=0.0, test_delay=0, start_day=0), # works, no diagnoses
+    5: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=0.5, loss_prob=0.0, test_delay=0, start_day=0), # works, ~80% diagnosed
+    6: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.5, test_delay=0, start_day=0), # works, ~80% diagnosed
+    7: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=0), # works, most diagnosed, slight mismatch due to new infections that day
+    8: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.0, test_delay=5, start_day=0), # works, most diagnosed, with delay, should be about one set of tick marks behind
+    9: cv.test_prob(symp_prob=1, asymp_prob=1, test_sensitivity=1.0, loss_prob=0.0, test_delay=0, start_day=7), # works, most diagnosed, with delay
+}
 
-sim.update_pars(interventions=testprob)
+sim.update_pars(interventions=testprobdict[case])
 
 to_plot = sc.dcp(cv.default_sim_plots)
 to_plot['Total counts'].append('cum_infectious')
