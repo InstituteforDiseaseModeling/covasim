46089f2be40298a93a3ce150bacf3a7140ecd280
cliffckerr <questionably@gmail.com>
2020-04-19 02:29:25 -0700

    refactor test

--- tests/devtests/run_viral_load.py
+++ tests/devtests/run_viral_load.py
@@ -5,7 +5,7 @@ import covasim as cv
 import numpy as np
 from scipy.special import kl_div
 
-runs = 301
+runs = 50
 r0_const = np.zeros(runs)
 r0_twolevel = np.zeros(runs)
 r0_twolevel2 = np.zeros(runs)
@@ -14,9 +14,9 @@ base_pars = sc.objdict(
         asymp_factor = 1, # Multiply beta by this factor for asymptomatic cases
         diag_factor  = 1, # Multiply beta by this factor for diganosed cases -- baseline assumes complete isolation
         verbose = 0,
-        pop_infected = 1
+        pop_size = 1000,
+        pop_infected = 10
 )
-base_pars['beta_dist']  = {'dist':'lognormal', 'par1':1, 'par2':0}
 base_pars['dur'] = {}
 base_pars['dur']['exp2inf']  = {'dist':'normal_int', 'par1':4, 'par2':0} # Duration from exposed to infectious
 base_pars['dur']['inf2sym']  = {'dist':'normal_int', 'par1':0, 'par2':0} # Duration from infectious to symptomatic
@@ -35,24 +35,24 @@ base_pars['rel_symp_prob']   = 2.0  # Scale factor for proportion of symptomatic
 base_pars['rel_severe_prob'] = 0  # Scale factor for proportion of symptomatic cases that become severe
 base_pars['rel_crit_prob']   = 0  # Scale factor for proportion of severe cases that become critical
 base_pars['rel_death_prob']  = 0  # Scale factor for proportion of critical cases that result in death
-base_pars['prog_by_age']     = False    
-    
+base_pars['prog_by_age']     = False
+
 for i in range(runs):
     # Configure the sim -- can also just use a normal dictionary
     pars = base_pars
     pars['rand_seed'] = i*np.random.rand()
-    pars['viral_dist'] = {'dist':'twolevel', 'par1':1, 'par2':1}
+    pars['viral_dist'] = {'frac_time':1, 'load_ratio':1}
     print('Making sim ', i, '...')
     sim1 = cv.Sim(pars=pars)
     sim1.run()
     r0_const[i] = len(sim1.people.transtree.targets[0])
     pars['rand_seed'] = i*np.random.rand()
-    pars['viral_dist'] = {'dist':'twolevel', 'par1':.5, 'par2':2}
+    pars['viral_dist'] = {'frac_time':.5, 'load_ratio':2}
     sim2 = cv.Sim(pars=pars)
     sim2.run()
     r0_twolevel[i] = len(sim2.people.transtree.targets[0])
     pars['rand_seed'] = i*np.random.rand()
-    pars['viral_dist'] = {'dist':'twolevel', 'par1':.3, 'par2':3}
+    pars['viral_dist'] = {'frac_time':.3, 'load_ratio':3}
     sim3 = cv.Sim(pars=pars)
     sim3.run()
     r0_twolevel2[i] = len(sim3.people.transtree.targets[0])
