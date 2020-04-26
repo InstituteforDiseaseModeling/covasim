b7d74f22bb931ec117d0d92d792958db1766464e
cliffckerr <questionably@gmail.com>
2020-04-26 12:58:26 -0700

    checking assumptions

--- tests/devtests/archive/pop_type_scaling.py
+++ tests/devtests/archive/pop_type_scaling.py
@@ -5,9 +5,9 @@ import numpy as np
 s1s = []
 s2s = []
 s3s = []
-ps = [5000, 10000, 20000, 50000, 100000][0]
+ps = [5000, 10000, 20000, 50000, 100000][2]
 pi = 100
-nd = 30
+nd = 40
 for i in [1, 2, 3]:
     print(i)
     s1 = cv.Sim(pop_size=ps, n_days=nd, pop_infected=pi, pop_type='random', rand_seed=i, verbose=0)
@@ -27,3 +27,13 @@ print(np.mean(s1s))
 print(np.mean(s2s))
 print(np.mean(s3s))
 
+s2.people.make_detailed_transtree()
+s3.people.make_detailed_transtree()
+tt_hy = s2.people.transtree.detailed
+tt_sp = s3.people.transtree.detailed
+
+for i,tt in enumerate([tt_hy, tt_sp]):
+    print(['hy', 'sp'][i])
+    for lk in 'hswc':
+        infs = [d for d in tt if d and d['layer']==lk]
+        print(lk, len(infs))
