94f61447e7b175eb68d8d32f83e9aab8536cbb16
cliffckerr <questionably@gmail.com>
2020-05-02 00:08:35 -0700

    moved scenarios plotting into shared library

--- covasim/plotting.py
+++ covasim/plotting.py
@@ -140,6 +140,129 @@ def plot(sim, to_plot=None, do_save=None, fig_path=None, fig_args=None, plot_arg
     return fig
 
 
+
+def plot_scens(scens, to_plot=None, do_save=None, fig_path=None, fig_args=None, plot_args=None,
+         scatter_args=None, axis_args=None, fill_args=None, legend_args=None, as_dates=True, dateformat=None,
+         interval=None, n_cols=1, font_size=18, font_family=None, grid=True, commaticks=True,
+         log_scale=False, do_show=True, sep_figs=False, verbose=None):
+    '''
+    Plot the results -- can supply arguments for both the figure and the plots.
+
+    Args:
+        scens        (Scens): The Scenarios object being plotted
+        to_plot      (dict):  Dict of results to plot; see get_scen_plots() for structure
+        do_save      (bool):  Whether or not to save the figure
+        fig_path     (str):   Path to save the figure
+        fig_args     (dict):  Dictionary of kwargs to be passed to pl.figure()
+        plot_args    (dict):  Dictionary of kwargs to be passed to pl.plot()
+        scatter_args (dict):  Dictionary of kwargs to be passed to pl.scatter()
+        axis_args    (dict):  Dictionary of kwargs to be passed to pl.subplots_adjust()
+        fill_args    (dict):  Dictionary of kwargs to be passed to pl.fill_between()
+        legend_args  (dict):  Dictionary of kwargs to be passed to pl.legend()
+        as_dates     (bool):  Whether to plot the x-axis as dates or time points
+        dateformat   (str):   Date string format, e.g. '%B %d'
+        interval     (int):   Interval between tick marks
+        n_cols       (int):   Number of columns of subpanels to use for subplot
+        font_size    (int):   Size of the font
+        font_family  (str):   Font face
+        grid         (bool):  Whether or not to plot gridlines
+        commaticks   (bool):  Plot y-axis with commas rather than scientific notation
+        log_scale    (bool):  Whether or not to plot the y-axis with a log scale; if a list, panels to show as log
+        do_show      (bool):  Whether or not to show the figure
+        sep_figs     (bool):  Whether to show separate figures for different results instead of subplots
+        verbose      (bool):  Display a bit of extra information
+
+    Returns:
+        fig: Figure handle
+    '''
+
+    if verbose is None:
+        verbose = scens['verbose']
+    sc.printv('Plotting...', 1, verbose)
+
+    if to_plot is None:
+        to_plot = cvd.get_scen_plots()
+    to_plot = sc.dcp(to_plot) # In case it's supplied as a dict
+
+    # Handle input arguments -- merge user input with defaults
+    fig_args    = sc.mergedicts({'figsize': (16, 14)}, fig_args)
+    plot_args   = sc.mergedicts({'lw': 3, 'alpha': 0.7}, plot_args)
+    scatter_args = sc.mergedicts({'s':70, 'marker':'s'}, scatter_args)
+    axis_args   = sc.mergedicts({'left': 0.10, 'bottom': 0.05, 'right': 0.95, 'top': 0.97, 'wspace': 0.25, 'hspace': 0.25}, axis_args)
+    fill_args   = sc.mergedicts({'alpha': 0.2}, fill_args)
+    legend_args = sc.mergedicts({'loc': 'best'}, legend_args)
+
+    sim = scens.base_sim
+
+    if sep_figs:
+        figs = []
+    else:
+        fig = pl.figure(**fig_args)
+    pl.subplots_adjust(**axis_args)
+    pl.rcParams['font.size'] = font_size
+    if font_family:
+        pl.rcParams['font.family'] = font_family
+
+    n_rows = np.ceil(len(to_plot)/n_cols) # Number of subplot rows to have
+    for rk,title,reskeys in to_plot.enumitems():
+        if sep_figs:
+            figs.append(pl.figure(**fig_args))
+            ax = pl.subplot(111)
+        else:
+            if rk == 0:
+                ax = pl.subplot(n_rows, n_cols, rk+1)
+            else:
+                ax = pl.subplot(n_rows, n_cols, rk+1, sharex=ax)
+
+        for reskey in reskeys:
+
+            resdata = scens.results[reskey]
+
+            for scenkey, scendata in resdata.items():
+
+                pl.fill_between(scens.tvec, scendata.low, scendata.high, **fill_args)
+                pl.plot(scens.tvec, scendata.best, label=scendata.name, **plot_args)
+                pl.title(title)
+                if rk == 0:
+                    pl.legend(**legend_args)
+
+                pl.grid(grid)
+                if commaticks:
+                    sc.commaticks()
+
+                if sim.data is not None and reskey in sim.data:
+                    data_t = np.array((sim.data.index-sim['start_day'])/np.timedelta64(1,'D'))
+                    pl.plot(data_t, sim.data[reskey], 'sk', **plot_args)
+
+                # Optionally reset tick marks (useful for e.g. plotting weeks/months)
+                if interval:
+                    xmin,xmax = ax.get_xlim()
+                    ax.set_xticks(pl.arange(xmin, xmax+1, interval))
+
+                # Set xticks as dates
+                if as_dates:
+                    @ticker.FuncFormatter
+                    def date_formatter(x, pos):
+                        return (sim['start_day'] + dt.timedelta(days=x)).strftime('%b-%d')
+                    ax.xaxis.set_major_formatter(date_formatter)
+                    if not interval:
+                        ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+    # Ensure the figure actually renders or saves
+    if do_save:
+        if fig_path is None: # No figpath provided - see whether do_save is a figpath
+            fig_path = 'covasim_scenarios.png' # Just give it a default name
+        fig_path = sc.makefilepath(fig_path) # Ensure it's valid, including creating the folder
+        pl.savefig(fig_path)
+
+    if do_show:
+        pl.show()
+    else:
+        pl.close(fig)
+
+    return fig
+
+
 def plot_result(sim, key, fig_args=None, plot_args=None):
     '''
     Simple method to plot a single result. Useful for results that aren't
