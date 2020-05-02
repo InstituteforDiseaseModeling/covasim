e79fbd398f2019ebeac912164c530a992a523cae
cliffckerr <questionably@gmail.com>
2020-05-01 20:13:48 -0700

    implementing multisim class

--- covasim/run.py
+++ covasim/run.py
@@ -17,7 +17,7 @@ from . import sim as cvs
 
 
 # Specify all externally visible functions this file defines
-__all__ = ['make_metapars', 'Scenarios', 'single_run', 'multi_run']
+__all__ = ['make_metapars', 'MultiSim', 'Scenarios', 'single_run', 'multi_run']
 
 
 
@@ -32,6 +32,364 @@ def make_metapars():
         verbose   = 1,
     )
     return metapars
+
+
+class MultiSim(sc.prettyobj):
+    '''
+    Class for running multiple copies of a simulation.
+
+    Args:
+        sims (Sim or list): a single sim or a list of sims
+        base_sim (Sim): the sim used for shared properties; if not supplied, the first of the sims provided
+
+    Returns:
+        msim: a MultiSim object
+
+    **Examples**::
+
+        sim = cv.Sim() # Create the sim
+        msim = cv.MultiSim(sim) # Create the multisim
+        msim.replicate(5) # Create 5 replicates
+        msim.run() # Run them in parallel
+        msim.reduce() # Calculate statistics
+        msim.plot() # Plot results
+
+        sims = [cv.Sim(beta=0.015*[1+0.1*i]) for i in range(5)] # Create sims
+        for sim in sims: sim.run() # Run sims in serial
+        msim = cv.MultiSim(sims) # Convert to multisim
+        msim.reduce('add') # Add results together
+        msim.plot() # Plot as single sim
+    '''
+
+    def __init__(self, sims=None, base_sim=None):
+
+        # Handle inputs
+        if base_sim is None:
+            if isinstance(sims, cvs.Sim):
+                base_sim = sims
+                sims = []
+            elif isinstance(sims, list):
+                base_sim = sims[0]
+            else:
+                errormsg = f'If base_sim is not supplied, sims must be either a sims or a list of sims, not {type(sims)}'
+                raise TypeError(errormsg)
+        if sims is None:
+            sims = []
+
+        self.sims = sims
+        self.base_sim = base_sim
+        self.results = None
+        self.which = None # Whether the multisim is to be reduced, combined, etc.
+
+        return
+
+
+    def __len__(self):
+        try:
+            return len(self.sims)
+        except:
+            return 0
+
+
+    def result_keys(self):
+        ''' Attempt to retrieve the results keys from the base sim '''
+        try:
+            keys = self.base_sim.result_keys()
+        except Exception as E:
+            errormsg = f'Could not retrieve result keys since base sim not accessible: {str(E)}'
+            raise ValueError(errormsg)
+        return keys
+
+
+    def run(self, *args, **kwargs):
+        '''
+        Run the actual scenarios
+
+        Args:
+            debug (bool): if True, runs a single run instead of multiple, which makes debugging easier
+            verbose (int): level of detail to print, passed to sim.run()
+            kwargs (dict): passed to multi_run() and thence to sim.run()
+
+        Returns:
+            None (modifies Scenarios object in place)
+        '''
+
+        self.sims = multi_run(self.sims, *args, **kwargs)
+        return
+
+
+    def combine(self, keep_people=False, output=False):
+        ''' Combine multiple sims into a single sim with scaled results '''
+
+        n_runs = len(self)
+        combined_sim = sc.dcp(self.sims[0])
+        combined_sim.parallelized = {'parallelized':True, 'combined':True, 'n_runs':n_runs}  # Store how this was parallelized
+        combined_sim['pop_size'] *= n_runs  # Record the number of people
+
+        for s,sim in enumerate(self.sims[1:]): # Skip the first one
+            if keep_people:
+                combined_sim.people += sim.people
+            for key in sim.result_keys():
+                this_res = sim.results[key]
+                combined_sim.results[key].values += this_res.values
+
+        # For non-count results (scale=False), rescale them
+        for key in combined_sim.result_keys():
+            if not combined_sim.results[key].scale:
+                combined_sim.results[key].values /= n_runs
+
+        self.combined_sim = combined_sim
+        self.results = self.combined_sim.results
+        self.which = 'combined'
+
+        if output:
+            return combined_sim
+        else:
+            return
+
+
+    def plot(self, to_plot=None, do_save=None, fig_path=None, fig_args=None, plot_args=None,
+             axis_args=None, fill_args=None, legend_args=None, as_dates=True, dateformat=None,
+             interval=None, n_cols=1, font_size=18, font_family=None, grid=True, commaticks=True,
+             do_show=True, sep_figs=False, verbose=None):
+        '''
+        Plot the results -- can supply arguments for both the figure and the plots.
+
+        Args:
+            to_plot     (dict): Dict of results to plot; see get_scen_plots() for structure
+            do_save     (bool): Whether or not to save the figure
+            fig_path    (str):  Path to save the figure
+            fig_args    (dict): Dictionary of kwargs to be passed to pl.figure()
+            plot_args   (dict): Dictionary of kwargs to be passed to pl.plot()
+            axis_args   (dict): Dictionary of kwargs to be passed to pl.subplots_adjust()
+            fill_args   (dict): Dictionary of kwargs to be passed to pl.fill_between()
+            legend_args (dict): Dictionary of kwargs to be passed to pl.legend()
+            as_dates    (bool): Whether to plot the x-axis as dates or time points
+            dateformat  (str):  Date string format, e.g. '%B %d'
+            interval    (int):  Interval between tick marks
+            n_cols      (int):  Number of columns of subpanels to use for subplot
+            font_size   (int):  Size of the font
+            font_family (str):  Font face
+            grid        (bool): Whether or not to plot gridlines
+            commaticks  (bool): Plot y-axis with commas rather than scientific notation
+            do_show     (bool): Whether or not to show the figure
+            sep_figs    (bool): Whether to show separate figures for different results instead of subplots
+            verbose     (bool): Display a bit of extra information
+
+        Returns:
+            fig: Figure handle
+        '''
+
+        if verbose is None:
+            verbose = self['verbose']
+        sc.printv('Plotting...', 1, verbose)
+
+        if to_plot is None:
+            to_plot = cvd.get_scen_plots()
+        to_plot = sc.dcp(to_plot) # In case it's supplied as a dict
+
+        # Handle input arguments -- merge user input with defaults
+        fig_args    = sc.mergedicts({'figsize': (16, 14)}, fig_args)
+        plot_args   = sc.mergedicts({'lw': 3, 'alpha': 0.7}, plot_args)
+        axis_args   = sc.mergedicts({'left': 0.10, 'bottom': 0.05, 'right': 0.95, 'top': 0.90, 'wspace': 0.25, 'hspace': 0.25}, axis_args)
+        fill_args   = sc.mergedicts({'alpha': 0.2}, fill_args)
+        legend_args = sc.mergedicts({'loc': 'best'}, legend_args)
+
+        if sep_figs:
+            figs = []
+        else:
+            fig = pl.figure(**fig_args)
+        pl.subplots_adjust(**axis_args)
+        pl.rcParams['font.size'] = font_size
+        if font_family:
+            pl.rcParams['font.family'] = font_family
+
+        n_rows = np.ceil(len(to_plot)/n_cols) # Number of subplot rows to have
+        for rk,reskey in enumerate(to_plot):
+            title = self.base_sim.results[reskey].name # Get the name of this result from the base simulation
+            if sep_figs:
+                figs.append(pl.figure(**fig_args))
+                ax = pl.subplot(111)
+            else:
+                if rk == 0:
+                    ax = pl.subplot(n_rows, n_cols, rk + 1)
+                else:
+                    ax = pl.subplot(n_rows, n_cols, rk + 1, sharex=ax)
+
+            resdata = self.results[reskey]
+
+            for scenkey, scendata in resdata.items():
+
+                pl.fill_between(self.tvec, scendata.low, scendata.high, **fill_args)
+                pl.plot(self.tvec, scendata.best, label=scendata.name, **plot_args)
+                pl.title(title)
+                if rk == 0:
+                    pl.legend(**legend_args)
+
+                pl.grid(grid)
+                if commaticks:
+                    sc.commaticks()
+
+                if self.base_sim.data is not None and reskey in self.base_sim.data:
+                    data_t = np.array((self.base_sim.data.index-self.base_sim['start_day'])/np.timedelta64(1,'D'))
+                    pl.plot(data_t, self.base_sim.data[reskey], 'sk', **plot_args)
+
+                # Optionally reset tick marks (useful for e.g. plotting weeks/months)
+                if interval:
+                    xmin,xmax = ax.get_xlim()
+                    ax.set_xticks(pl.arange(xmin, xmax+1, interval))
+
+                # Set xticks as dates
+                if as_dates:
+                    @ticker.FuncFormatter
+                    def date_formatter(x, pos):
+                        return (self.base_sim['start_day'] + dt.timedelta(days=x)).strftime('%b-%d')
+                    ax.xaxis.set_major_formatter(date_formatter)
+                    if not interval:
+                        ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+
+        # Ensure the figure actually renders or saves
+        if do_save:
+            if fig_path is None: # No figpath provided - see whether do_save is a figpath
+                fig_path = 'covasim_scenarios.png' # Just give it a default name
+            fig_path = sc.makefilepath(fig_path) # Ensure it's valid, including creating the folder
+            pl.savefig(fig_path)
+
+        if do_show:
+            pl.show()
+        else:
+            pl.close(fig)
+
+        return fig
+
+
+    def to_json(self, filename=None, tostring=True, indent=2, verbose=False, *args, **kwargs):
+        '''
+        Export results as JSON.
+
+        Args:
+            filename (str): if None, return string; else, write to file
+
+        Returns:
+            A unicode string containing a JSON representation of the results,
+            or writes the JSON file to disk
+
+        '''
+        d = {'t':self.tvec,
+             'results':   self.results,
+             'basepars':  self.basepars,
+             'metapars':  self.metapars,
+             'simpars':   self.base_sim.export_pars(),
+             'scenarios': self.scenarios
+             }
+        if filename is None:
+            output = sc.jsonify(d, tostring=tostring, indent=indent, verbose=verbose, *args, **kwargs)
+        else:
+            output = sc.savejson(filename=filename, obj=d, indent=indent, *args, **kwargs)
+
+        return output
+
+
+    def to_excel(self, filename=None):
+        '''
+        Export results as XLSX
+
+        Args:
+            filename (str): if None, return string; else, write to file
+
+        Returns:
+            An sc.Spreadsheet with an Excel file, or writes the file to disk
+
+        '''
+        spreadsheet = sc.Spreadsheet()
+        spreadsheet.freshbytes()
+        with pd.ExcelWriter(spreadsheet.bytes, engine='xlsxwriter') as writer:
+            for key in self.result_keys():
+                result_df = pd.DataFrame.from_dict(sc.flattendict(self.results[key], sep='_'))
+                result_df.to_excel(writer, sheet_name=key)
+        spreadsheet.load()
+
+        if filename is None:
+            output = spreadsheet
+        else:
+            output = spreadsheet.save(filename)
+
+        return output
+
+
+    def save(self, scenfile=None, keep_sims=True, keep_people=False, **kwargs):
+        '''
+        Save to disk as a gzipped pickle.
+
+        Args:
+            scenfile (str or None): the name or path of the file to save to; if None, uses stored
+            keep_sims (bool): whether or not to store the actual Sim objects in the Scenarios object
+            keep_people (bool): whether or not to store the population in the Sim objects (NB, very large)
+            keywords: passed to makefilepath()
+
+        Returns:
+            scenfile (str): the validated absolute path to the saved file
+
+        **Example**::
+
+            scens.save() # Saves to a .scens file with the date and time of creation by default
+
+        '''
+        if scenfile is None:
+            scenfile = self.scenfile
+        scenfile = sc.makefilepath(filename=scenfile, **kwargs)
+        self.scenfile = scenfile # Store the actual saved filename
+
+        # Store sims seperately
+        sims = self.sims
+        self.sims = None # Remove for now
+
+        obj = sc.dcp(self) # This should be quick once we've removed the sims
+        if not keep_people:
+            obj.base_sim.shrink(in_place=True)
+
+        if keep_sims or keep_people:
+            if keep_people:
+                if not obj._kept_people:
+                    print('Warning: there are no people because they were not saved during the run. '
+                          'If you want people, please rerun with keep_people=True.')
+                obj.sims = sims # Just restore the object in full
+                print('Note: saving people, which may produce a large file!')
+            else:
+                obj.sims = sc.objdict()
+                for key in sims.keys():
+                    obj.sims[key] = []
+                    for sim in sims[key]:
+                        obj.sims[key].append(sim.shrink(in_place=False))
+
+        sc.saveobj(filename=scenfile, obj=obj) # Actually save
+
+        self.sims = sims # Restore
+        return scenfile
+
+
+    @staticmethod
+    def load(scenfile, *args, **kwargs):
+        '''
+        Load from disk from a gzipped pickle.
+
+        Args:
+            scenfile (str): the name or path of the file to save to
+            kwargs: passed to sc.loadobj()
+
+        Returns:
+            scens (Scenarios): the loaded scenarios object
+
+        **Example**::
+
+            scens = cv.Scenarios.load('my-scenarios.scens')
+        '''
+        scens = cvm.load(scenfile, *args, **kwargs)
+        if not isinstance(scens, Scenarios):
+            errormsg = f'Cannot load object of {type(scens)} as a Scenarios object'
+            raise TypeError(errormsg)
+        return scens
+
 
 
 class Scenarios(cvb.ParsObj):
@@ -577,26 +935,4 @@ def multi_run(sim, n_runs=4, noise=0.0, noisepar=None, iterpars=None, verbose=No
         errormsg = f'Must be Sim object or list, not {type(sim)}'
         raise TypeError(errormsg)
 
-    # Usual case -- return a list of sims
-    if not combine:
-        return sims
-
-    # Or, combine them into a single sim with scaled results
-    else:
-        output_sim = sc.dcp(sims[0])
-        output_sim.parallelized = {'parallelized':True, 'combined':True, 'n_runs':n_runs}  # Store how this was parallelized
-        output_sim['pop_size'] *= n_runs  # Record the number of people
-
-        for s,sim in enumerate(sims[1:]): # Skip the first one
-            if keep_people:
-                output_sim.people += sim.people
-            for key in sim.result_keys():
-                this_res = sim.results[key]
-                output_sim.results[key].values += this_res.values
-
-        # For non-count results (scale=False), rescale them
-        for key in output_sim.result_keys():
-            if not output_sim.results[key].scale:
-                output_sim.results[key].values /= len(sims)
-
-        return output_sim
+    return sims
--- tests/test_run.py
+++ tests/test_run.py
@@ -75,7 +75,7 @@ def test_scenarios(do_plot=False):
     return scens
 
 
-def test_combine(do_plot=False): # If being run via pytest, turn off
+def test_multisim_combine(do_plot=False): # If being run via pytest, turn off
     sc.heading('Combine results test')
 
     n_runs = 3
@@ -83,19 +83,22 @@ def test_combine(do_plot=False): # If being run via pytest, turn off
     pop_infected = 10
 
     print('Running first sim...')
-    sim = cv.Sim({'pop_size':pop_size, 'pop_infected':pop_infected})
-    sim = cv.multi_run(sim=sim, n_runs=n_runs, combine=True, keep_people=True)
-    assert sim['pop_size'] == pop_size*n_runs
+    sim = cv.Sim(pop_size=pop_size, pop_infected=pop_infected)
+    msim = cv.MultiSim(sim)
+    msim.replicate(n_runs)
+    msim.run()
+    sim1 = msim.combine(output=True, keep_people=True)
+    assert sim1['pop_size'] == pop_size*n_runs
 
     print('Running second sim, results should be similar but not identical (stochastic differences)...')
-    sim2 = cv.Sim({'pop_size':pop_size*n_runs, 'pop_infected':pop_infected*n_runs})
+    sim2 = cv.Sim(pop_size=pop_size*n_runs, pop_infected=pop_infected*n_runs)
     sim2.run()
 
     if do_plot:
-        sim.plot()
+        msim.plot()
         sim2.plot()
 
-    return sim
+    return msim
 
 
 #%% Run as a script
@@ -103,8 +106,10 @@ if __name__ == '__main__':
     T = sc.tic()
 
     # sim1  = test_singlerun()
-    sims2 = test_multirun(do_plot=do_plot)
-    # sims3 = test_combine(do_plot=do_plot)
+    # sims2 = test_multirun(do_plot=do_plot)
+    # sims3 = test_multisim_stats(do_plot=do_plot)
+    sims4 = test_multisim_combine(do_plot=do_plot)
+
     # scens = test_scenarios(do_plot=do_plot)
 
     sc.toc(T)
