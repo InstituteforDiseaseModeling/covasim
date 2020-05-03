b3b434ee68139d3e1cfb1ea7c96a6ca2b12da6a6
cliffckerr <questionably@gmail.com>
2020-05-02 20:24:21 -0700

    minor test update

--- tests/devtests/archive/multisim_tests.py
+++ tests/devtests/archive/multisim_tests.py
@@ -1,18 +1,18 @@
 import covasim as cv
 
 sim = cv.Sim() # Create the sim
-msim = cv.MultiSim(sim, n_runs=5) # Create the multisim
-msim.run() # Run them in parallel
-msim.combine() # Combine into one sim
-msim.plot() # Plot results
+msim1 = cv.MultiSim(sim, n_runs=5) # Create the multisim
+msim1.run() # Run them in parallel
+msim1.combine() # Combine into one sim
+msim1.plot() # Plot results
 
 sim = cv.Sim() # Create the sim
-msim = cv.MultiSim(sim, n_runs=11, noise=0.1) # Set up a multisim with noise
-msim.run() # Run
-msim.reduce() # Compute statistics
-msim.plot() # Plot
+msim2 = cv.MultiSim(sim, n_runs=11, noise=0.1) # Set up a multisim with noise
+msim2.run() # Run
+msim2.reduce() # Compute statistics
+msim2.plot() # Plot
 
 sims = [cv.Sim(beta=0.015*(1+0.02*i)) for i in range(5)] # Create sims
 for sim in sims: sim.run() # Run sims in serial
-msim = cv.MultiSim(sims) # Convert to multisim
-msim.plot() # Plot as single sim
+msim3 = cv.MultiSim(sims) # Convert to multisim
+msim3.plot() # Plot as single sim
