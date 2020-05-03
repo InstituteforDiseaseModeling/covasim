d668758a36a7768d7280c4287e0f4f308709d688
cliffckerr <questionably@gmail.com>
2020-05-03 13:18:24 -0700

    looking into numbers of tests as well

--- tests/devtests/dev_test_rescaling.py
+++ tests/devtests/dev_test_rescaling.py
@@ -19,8 +19,9 @@ tp = cv.test_prob(start_day=iday, symp_prob=0.1, asymp_prob=0.01) # Test a numbe
 
 # Properties that are shared across sims
 basepop      = 10e3
-popscale     = 10
 popinfected  = 20
+popscale1    = 10
+popscale2    = 20 # Try a different population scale
 which_interv = 2 # Which intervention to test
 
 shared = sc.objdict(
@@ -31,7 +32,7 @@ shared = sc.objdict(
 
 # Simulate the entire population
 p.entire = dict(
-    pop_size     = basepop*popscale,
+    pop_size     = basepop*popscale1,
     pop_infected = popinfected,
     pop_scale    = 1,
     rescale      = False,
@@ -41,18 +42,44 @@ p.entire = dict(
 p.rescale = dict(
     pop_size     = basepop,
     pop_infected = popinfected,
-    pop_scale    = popscale,
+    pop_scale    = popscale1,
     rescale      = True,
 )
 
 # Simulate a small population with static scaling
 p.static = dict(
     pop_size     = basepop,
-    pop_infected = popinfected//popscale,
-    pop_scale    = popscale,
+    pop_infected = popinfected//popscale1,
+    pop_scale    = popscale1,
     rescale      = False,
 )
 
+# Simulate an extra large population
+p.entire2 = dict(
+    pop_size     = basepop*popscale2,
+    pop_infected = popinfected,
+    pop_scale    = 1,
+    rescale      = False,
+)
+
+# Simulate a small population with dynamic scaling
+p.rescale2 = dict(
+    pop_size     = basepop,
+    pop_infected = popinfected,
+    pop_scale    = popscale2,
+    rescale      = True,
+)
+
+# Simulate a small population with static scaling
+p.static2 = dict(
+    pop_size     = basepop,
+    pop_infected = popinfected//popscale2,
+    pop_scale    = popscale2,
+    rescale      = False,
+)
+
+
+# Create and run the sims
 keys = p.keys()
 
 for key in keys:
@@ -65,7 +92,16 @@ for key in keys:
 for key in keys:
     m[key].run()
     m[key].reduce()
-    m[key].plot()
+
+
+# Plot
+to_plot = {
+    'Totals': ['cum_infections', 'cum_tests', 'cum_diagnoses'],
+    'New': ['new_infections', 'new_tests', 'new_diagnoses'],
+    }
+
+for key in keys:
+    m[key].plot(to_plot=to_plot)
 
 bsims = [msim.base_sim for msim in m.values()]
 mm = cv.MultiSim(sims=bsims)
