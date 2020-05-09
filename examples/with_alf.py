89b99fb3814f5504cec37db36643bac78397510c
Jamie Cohen <jamie.alex.cohen@gmail.com>
2020-05-09 12:37:42 -0400

    removing health care aides from workplace contacts

--- examples/with_alf.py
+++ examples/with_alf.py
@@ -1,20 +1,26 @@
 import covasim as cv
 import sciris as sc
 import numpy as np
+import pandas as pd
+from covasim import base as cvb
+
 sim = cv.Sim(pop_type='hybrid', pop_size=2e4)
 sim.initialize()
 
-# Define ALF parameters
-# According to LTC 2015-2016 data for all of Washington:
-# 83% of residents in nursing homes are 65> and 95% of residents in residential care communities are 65>.
-# Therefore we set the minimum age to 70.
-# The average size of a nursing home is 76 and average size of a residential care community is 18.
-# There are 200 nursing homes in all of Washington and 2,000 residential care communities, so we
-# set the average size of an ALF to 25.
-# There are approximately 51,200 people residing in ALFs in Washington. We compared this to the number of
-# people living above 60 in Washington State (132,021) to conclude that 38% of these people are living in ALFs.
-# In the 2,200 combined facilities, there are 23,569 nursing and social work staff employees.
-# Therefore, we assume each facility has on average 10 health care staff.
+''' 
+Define ALF parameters
+According to LTC 2015-2016 data for all of Washington:
+83% of residents in nursing homes are 65> and 95% of residents in residential care communities are 65>.
+Therefore we set the minimum age to 70.
+The average size of a nursing home is 76 and average size of a residential care community is 18.
+There are 200 nursing homes in all of Washington and 2,000 residential care communities, so we
+set the average size of an ALF to 25.
+There are approximately 51,200 people residing in ALFs in Washington. We compared this to the number of
+people living above 60 in Washington State (132,021) to conclude that 38% of these people are living in ALFs.
+In the 2,200 combined facilities, there are 23,569 nursing and social work staff employees.
+Therefore, we assume each facility has on average 10 health care staff.
+
+'''
 
 min_alf_age = 70
 n_alf_contacts = 25
@@ -32,14 +38,14 @@ pop_size = sim.pars['pop_size']
 layer_keys = ['alf']
 alf_inds = cv.binomial_filter(alf_prob, sc.findinds(sim.people.age >= min_alf_age))
 assert max(alf_inds) < pop_size
-contacts_list = [{key:[] for key in layer_keys} for i in range(pop_size)]
-alf_contacts, _ = cv.make_microstructured_contacts(len(alf_inds), {'alf':n_alf_contacts}, sim, directed=True)
+contacts_list = [{key: [] for key in layer_keys} for i in range(pop_size)]
+alf_contacts, _ = cv.make_microstructured_contacts(len(alf_inds), {'alf': n_alf_contacts}, sim)
 
 # get the dictionary of ALFs
 alf_dict = sim.contactdict['alf']
 
 for i, ind in enumerate(alf_inds):
-    contacts_list[ind]['alf'] = alf_inds[alf_contacts[i]['alf']].tolist() # Copy over alf contacts
+    contacts_list[ind]['alf'] = alf_inds[alf_contacts[i]['alf']].tolist()  # Copy over alf contacts
     if len(contacts_list[ind]['alf']):
         assert max(contacts_list[ind]['alf']) < pop_size
 
@@ -50,17 +56,21 @@ for id, alf_members in alf_dict.items():
 
 # Create list of health care workers
 health_care_worker_age = [22, 55]
-health_care_worker_inds = sc.findinds((sim.people.age >= health_care_worker_age[0]) * (sim.people.age < health_care_worker_age[1]))
+health_care_worker_inds = sc.findinds(
+    (sim.people.age >= health_care_worker_age[0]) * (sim.people.age < health_care_worker_age[1]))
 assert max(health_care_worker_inds) < pop_size
 
 # Loop through all ALFs, select a number of health care workers from health_care_worker_inds, add them to ALF and
-# to list of contacts, then remove them from health_care_worker_inds
+# to list of contacts, then remove them from health_care_worker_inds; remove them from workplace and workplace contacts.
+
+health_care_aides = [] # List of health care aides. will use this later to clip their edges in the workplace
 
 for id, cluster_members in alf_dict.items():
     num_health_care_workers = cv.poisson(n_alf_staff)
     health_care_workers_to_add = np.random.choice(health_care_worker_inds, n_alf_staff, replace=False).tolist()
     assert max(health_care_workers_to_add) < pop_size
     for person in health_care_workers_to_add:
+        health_care_aides.append(person)
         contacts_list[person]['alf'] += cluster_members
         assert max(contacts_list[person]['alf']) < pop_size
         cluster_members.append(person)
@@ -70,6 +80,18 @@ for id, cluster_members in alf_dict.items():
     for person in range(len(health_care_workers_to_add)):
         health_care_worker_inds = health_care_worker_inds[health_care_worker_inds != health_care_workers_to_add[person]]
         assert max(health_care_worker_inds) < pop_size
+
+health_care_aides = health_care_aides
+work_layer = sim.people.contacts['w']
+work_layer_df = pd.DataFrame(work_layer)
+for aide in health_care_aides:
+    work_layer_df.drop(work_layer_df[work_layer_df["p1"] == aide].index, inplace=True)
+    work_layer_df.drop(work_layer_df[work_layer_df["p2"] == aide].index, inplace=True)
+
+print("aides dropped from work layer")
+new_work_layer = cvb.Layer().from_df(work_layer_df) # Convert back
+new_work_layer.validate()
+sim.people.contacts['w'] = new_work_layer
 
 sim.contactdict['alf'] = alf_dict
 sim.people.add_contacts(contacts_list, lkey='alf')
