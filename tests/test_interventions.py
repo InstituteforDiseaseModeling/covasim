af24d8abae8c0daedef0c1a599123035ea08101f
cliffckerr <questionably@gmail.com>
2020-05-08 22:19:31 -0700

    merged interventions tests

--- tests/test_interventions.py
+++ tests/test_interventions.py
@@ -3,6 +3,7 @@ Testing the effect of testing interventions in Covasim
 '''
 
 #%% Imports and settings
+import os
 import sciris as sc
 import covasim as cv
 
@@ -12,6 +13,7 @@ do_save   = 0
 debug     = 1
 keep_sims = 0
 fig_paths = [f'results/testing_scen_{i}.png' for i in range(3)]
+fig_paths += ['results/testing_other.png']
 
 
 def test_interventions(do_plot=False, do_show=True, do_save=False, fig_path=None):
@@ -101,7 +103,7 @@ def test_turnaround(do_plot=False, do_show=True, do_save=False, fig_path=None):
     n_runs = 3
     verbose = 1
     base_pars = {
-      'pop_size': 5000,
+      'pop_size': 1000,
       'pop_type': 'hybrid',
       }
 
@@ -147,7 +149,7 @@ def test_tracedelay(do_plot=False, do_show=True, do_save=False, fig_path=None):
     n_runs = 3
     verbose = 1
     base_pars = {
-      'pop_size': 2000,
+      'pop_size': 1000,
       'pop_type': 'hybrid',
       }
 
@@ -227,6 +229,130 @@ def test_tracedelay(do_plot=False, do_show=True, do_save=False, fig_path=None):
 
 
 
+def test_beta_edges(do_plot=False, do_show=True, do_save=False, fig_path=None):
+
+    pars = dict(
+        pop_size=1000,
+        pop_infected=20,
+        pop_type='hybrid',
+        )
+
+    start_day = 25 # Day to start the intervention
+    end_day   = 40 # Day to end the intervention
+    change    = 0.3 # Amount of change
+
+    sims = sc.objdict()
+    sims.b = cv.Sim(pars) # Beta intervention
+    sims.e = cv.Sim(pars) # Edges intervention
+
+    beta_interv = cv.change_beta(days=[start_day, end_day], changes=[change, 1.0])
+    edge_interv = cv.clip_edges(start_day=start_day, end_day=end_day, change=change, verbose=True)
+    sims.b.update_pars(interventions=beta_interv)
+    sims.e.update_pars(interventions=edge_interv)
+
+    for sim in sims.values():
+        sim.run()
+        if do_plot:
+            sim.plot(do_save=do_save, do_show=do_show, fig_path=fig_path)
+            sim.plot_result('r_eff')
+
+    return sims
+
+
+def test_beds(do_plot=False, do_show=True, do_save=False, fig_path=None):
+    sc.heading('Test of bed capacity estimation')
+
+    sc.heading('Setting up...')
+
+    sc.tic()
+
+    n_runs = 2
+    verbose = 1
+
+    basepars = {'pop_size': 1000}
+    metapars = {'n_runs': n_runs}
+
+    sim = cv.Sim()
+
+    # Define the scenarios
+    scenarios = {
+        'baseline': {
+          'name': 'No bed constraints',
+          'pars': {
+              'pop_infected': 100
+          }
+        },
+        'bedconstraint': {
+            'name': 'Only 50 beds available',
+            'pars': {
+                'pop_infected': 100,
+                'n_beds': 50,
+            }
+        },
+        'bedconstraint2': {
+            'name': 'Only 10 beds available',
+            'pars': {
+                'pop_infected': 100,
+                'n_beds': 10,
+            }
+        },
+    }
+
+    scens = cv.Scenarios(sim=sim, basepars=basepars, metapars=metapars, scenarios=scenarios)
+    scens.run(verbose=verbose, debug=debug)
+
+    if do_plot:
+        to_plot = sc.odict({
+            'Cumulative deaths':   'cum_deaths',
+            'People needing beds / beds': 'bed_capacity',
+            'Number of cases requiring hospitalization': 'n_severe',
+            'Number of cases requiring ICU': 'n_critical',
+        })
+        scens.plot(to_plot=to_plot, do_save=do_save, do_show=do_show, fig_path=fig_path)
+
+    return scens
+
+
+def test_borderclosure(do_plot=False, do_show=True, do_save=False, fig_path=None):
+    sc.heading('Test effect of border closures')
+
+    sc.heading('Setting up...')
+
+    sc.tic()
+
+    n_runs = 2
+    verbose = 1
+
+    basepars = {'pop_size': 1000}
+    basepars = {'n_imports': 5}
+    metapars = {'n_runs': n_runs}
+
+    sim = cv.Sim()
+
+    # Define the scenarios
+    scenarios = {
+        'baseline': {
+            'name': 'No border closures',
+            'pars': {
+            }
+        },
+        'borderclosures_day10': {
+            'name': 'Close borders on day 10',
+            'pars': {
+                'interventions': [cv.dynamic_pars({'n_imports': {'days': 10, 'vals': 0}})]
+            }
+        },
+    }
+
+    scens = cv.Scenarios(sim=sim, basepars=basepars, metapars=metapars, scenarios=scenarios)
+    scens.run(verbose=verbose, debug=debug)
+
+    if do_plot:
+        scens.plot(do_save=do_save, do_show=do_show, fig_path=fig_path)
+
+    return scens
+
+
 #%% Run as a script
 if __name__ == '__main__':
     sc.tic()
@@ -234,6 +360,14 @@ if __name__ == '__main__':
     scens1 = test_interventions(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_paths[0])
     scens2 = test_turnaround(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_paths[1])
     scens3 = test_tracedelay(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_paths[2])
+    sims = test_beta_edges(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_paths[3])
+    bed_scens = test_beds(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_paths[3])
+    border_scens = test_borderclosure(do_plot=do_plot, do_save=do_save, do_show=do_show, fig_path=fig_paths[3])
+
+    for path in fig_paths:
+        if os.path.exists(path):
+            print(f'Removing {path}')
+            os.remove(path)
 
     sc.toc()
 
