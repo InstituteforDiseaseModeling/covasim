0abe6d3435804d46901e5971138ab2eb83aca816
cliffckerr <questionably@gmail.com>
2020-05-14 00:12:04 -0700

    comparison

--- tests/devtests/ttq_interventions.py
+++ tests/devtests/ttq_interventions.py
@@ -25,11 +25,20 @@ pars = dict(
     n_days = 300,
     )
 
-intervs = [
-    cv.test_prob(start_day=10, symp_prob=0.1, asymp_prob=0.001, symp_quar_prob=0.8, asymp_quar_prob=0.0, test_delay=1.0, do_plot=False),
-    cv.contact_tracing(start_day=10, trace_probs=dict(h=0.9, s=0.7, w=0.7, c=0.3), trace_time=2.0, presumptive=False, do_plot=False),
-    ]
+sims = []
 
-sim = cv.Sim(pars, interventions=intervs)
-sim.run()
-plot_all(sim)
+for aqp in [0, 1.0]:
+    for pr in [False, True]:
+
+        intervs = [
+            cv.test_prob(start_day=10, symp_prob=0.1, asymp_prob=0.001, symp_quar_prob=0.8, asymp_quar_prob=aqp, test_delay=1.0, do_plot=False),
+            cv.contact_tracing(start_day=10, trace_probs=dict(h=0.9, s=0.7, w=0.7, c=0.3), trace_time=2.0, presumptive=pr, do_plot=False),
+            ]
+
+        sim = cv.Sim(pars, interventions=intervs)
+        sim.run()
+        plot_all(sim)
+        sims.append(sim)
+
+msim = cv.MultiSim(sims)
+msim.plot()
