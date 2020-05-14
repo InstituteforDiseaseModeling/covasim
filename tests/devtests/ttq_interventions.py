83e80508c10058471a9b8bdf900756356cfbb8fb
cliffckerr <questionably@gmail.com>
2020-05-14 00:23:43 -0700

    exploring simple scenarios

--- tests/devtests/ttq_interventions.py
+++ tests/devtests/ttq_interventions.py
@@ -21,24 +21,32 @@ def plot_all(sim):
 pars = dict(
     pop_size = 20e3,
     pop_type = 'hybrid',
-    rescale = True,
+    rescale = False,
     n_days = 300,
     )
 
-sims = []
+intervs = [
+    cv.test_prob(start_day=20, symp_prob=1.0, asymp_prob=0.0, symp_quar_prob=0.0, asymp_quar_prob=0.0, test_delay=1.0),
+    cv.contact_tracing(start_day=20, trace_probs=1.0, trace_time=1.0, presumptive=True),
+    ]
 
-for aqp in [0, 1.0]:
-    for pr in [False, True]:
+sim = cv.Sim(pars, interventions=intervs)
+sim.run()
+plot_all(sim)
 
-        intervs = [
-            cv.test_prob(start_day=10, symp_prob=0.1, asymp_prob=0.001, symp_quar_prob=0.8, asymp_quar_prob=aqp, test_delay=1.0, do_plot=False),
-            cv.contact_tracing(start_day=10, trace_probs=dict(h=0.9, s=0.7, w=0.7, c=0.3), trace_time=2.0, presumptive=pr, do_plot=False),
-            ]
 
-        sim = cv.Sim(pars, interventions=intervs)
-        sim.run()
-        plot_all(sim)
-        sims.append(sim)
+# for aqp in [0, 1.0]:
+#     for pr in [False, True]:
 
-msim = cv.MultiSim(sims)
-msim.plot()
+#         intervs = [
+#             cv.test_prob(start_day=10, symp_prob=0.1, asymp_prob=0.001, symp_quar_prob=0.0, asymp_quar_prob=0, test_delay=1.0, do_plot=False),
+#             cv.contact_tracing(start_day=10, trace_probs=dict(h=0.9, s=0.7, w=0.7, c=0.3), trace_time=2.0, presumptive=pr, do_plot=False),
+#             ]
+
+#         sim = cv.Sim(pars, interventions=intervs)
+#         sim.run()
+#         plot_all(sim)
+#         sims.append(sim)
+
+# msim = cv.MultiSim(sims)
+# msim.plot()
