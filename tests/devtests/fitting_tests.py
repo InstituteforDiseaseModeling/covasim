69d9d5841799ddf0cd298462aebb790b73209c01
cliffckerr <questionably@gmail.com>
2020-05-29 03:47:24 -0700

    try different test values

--- tests/devtests/fitting_tests.py
+++ tests/devtests/fitting_tests.py
@@ -8,15 +8,15 @@ regenerate = 0
 datafile = 'target_fit_data.xlsx'
 # datafile = '../example_data.csv'
 
-intervs = []#[cv.change_beta(days=40, changes=0.5), cv.test_prob(start_day=20, symp_prob=0.1, asymp_prob=0.01)] # Common interventions
+intervs = [cv.change_beta(days=40, changes=0.5), cv.test_prob(start_day=20, symp_prob=0.1, asymp_prob=0.01)] # Common interventions
 pars = dict(
     pop_size      = 20000,    # Population size
-    pop_infected  = 100,      # Number of initial infections -- use more for increased robustness
+    pop_infected  = 80,      # Number of initial infections -- use more for increased robustness
     pop_type      = 'hybrid', # Population to use -- "hybrid" is random with household, school,and work structure
     verbose       = 0,        # Don't print details of the run
     interventions = intervs,   # Include the most common interventions
-    rand_seed     = 1,
-    beta          = 0.015,
+    rand_seed     = 2,
+    beta          = 0.017,
 )
 
 target_pars = dict(
@@ -33,7 +33,7 @@ if regenerate:
 
 sim = cv.Sim(pars, datafile=datafile)
 sim.run()
-# sim.plot()
-fit = sim.compute_fit(keys=['cum_deaths', 'cum_diagnoses', 'new_infections', 'new_diagnoses'])
-# print(fit.mismatch)
+fit = sim.compute_fit()
+# fit = sim.compute_fit(keys=['cum_deaths', 'cum_diagnoses', 'new_infections', 'new_diagnoses'])
+print('Mismatch: ', fit.mismatch)
 fit.plot()
