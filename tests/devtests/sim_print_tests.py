7a564c23c096091d8d87751a310b323408ac2f9d
cliffckerr <questionably@gmail.com>
2020-05-29 00:58:39 -0700

    improve example

--- tests/devtests/sim_print_tests.py
+++ tests/devtests/sim_print_tests.py
@@ -1,19 +1,23 @@
+'''
+Illustrate different ways of printing a sim.
+'''
+
 import sciris as sc
 import covasim as cv
 
-s = cv.Sim(label='sim1', verbose=0)
-s.run()
+sim = cv.Sim(label='sim1', verbose=0)
+sim.run()
 
-sc.heading('print')
-print(s)
+sc.heading('print(sim)')
+print(sim)
 
-sc.heading('summarize')
-s.summarize()
+sc.heading('sim.summarize()')
+sim.summarize()
 
-sc.heading('brief')
-s.brief()
+sc.heading('sim.brief()')
+sim.brief()
 
-sc.heading('multisim')
-msim = cv.MultiSim(s, verbose=0)
+sc.heading('msim.summarize()')
+msim = cv.MultiSim(sim, verbose=0)
 msim.run(reduce=True)
 msim.summarize()
