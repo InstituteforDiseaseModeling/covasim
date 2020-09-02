c42094f240097d88f777ba101cdfe5be9732c4eb
cliffckerr <questionably@gmail.com>
2020-09-02 00:37:01 -0700

    more test refactoring

--- tests/test_resume.py
+++ tests/test_resume.py
@@ -3,6 +3,7 @@ Test partial simulation
 '''
 
 #%% Imports and settings
+import os
 import pytest
 import numpy as np
 import sciris as sc
@@ -18,7 +19,7 @@ pars = dict(pop_size=1e3, verbose=0)
 def test_resuming():
     sc.heading('Test that resuming a run works')
 
-    s0 = cv.Sim(**pars, start_day='2020-01-01')
+    s0 = cv.Sim(pars, start_day='2020-01-01')
     s1 = s0.copy()
     s0.run()
 
@@ -31,7 +32,7 @@ def test_resuming():
         s1.run(until=0)
     assert s1.initialized # It should still have been initialized though
 
-    s1.run(until='2020-01-30', reset_seed=False)
+    s1.run(until='2020-01-31', reset_seed=False)
     with pytest.raises(cv.AlreadyRunError):
         s1.run(until=30) # Error if running up to the same value
     with pytest.raises(cv.AlreadyRunError):
@@ -47,7 +48,7 @@ def test_resuming():
 
 def test_reset_seed():
     sc.heading('Test that resetting a simulation works')
-    s0 = cv.Sim(**pars)
+    s0 = cv.Sim(pars)
     s1 = s0.copy()
     s0.run()
 
@@ -64,8 +65,10 @@ def test_reset_seed():
 def test_reproducibility():
     sc.heading('Test that sims are reproducible')
 
+    fn = 'save-load-test.sim' # Name of the test file to save
+
     #The results of the sim shouldn't be affected by what you do or don't do prior to sim.run()
-    s1 = cv.Sim(**pars)
+    s1 = cv.Sim(pars)
     s1.initialize()
     s2 = s1.copy()
     s1.run()
@@ -75,47 +78,40 @@ def test_reproducibility():
     assert r1ci == r2ci
 
     # If you run a sim and save it, you should be able to re-run it on load
-    fn = 'save-load-test.sim'
-    s1 = cv.Sim(**pars)
-    s1.run()
-    s1.save(fn)
-    s2 = cv.load(fn)
-    s2.initialize()
-    s2.run()
-    r1ci = s1.summary['cum_infections']
-    r2ci = s2.summary['cum_infections']
-    assert r1ci == r2ci
+    s3 = cv.Sim(pars)
+    s3.run()
+    s3.save(fn)
+    s4 = cv.load(fn)
+    s4.initialize()
+    s4.run()
+    r3ci = s3.summary['cum_infections']
+    r4ci = s4.summary['cum_infections']
+    assert r3ci == r4ci
+    if os.path.exists(fn): # Tidy up -- after the assert to allow inspection if it fails
+        os.remove(fn)
 
-    return s2
+    return s4
 
 
-def test_start_stop(): # If being run via pytest, turn off
+def test_step(): # If being run via pytest, turn off
     sc.heading('Test starting and stopping')
 
-    pars = {'pop_size': 1000}
-
     # Create and run a basic simulation
-    sim1 = cv.Sim(pars)
-    sim1.run(verbose=0)
+    s1 = cv.Sim(pars)
+    s1.run(verbose=0)
 
     # Test that step works
-    sim2 = cv.Sim(pars)
-    sim2.initialize()
-    for n in range(sim2.npts):
-        sim2.step()
-    sim2.finalize()
-
-    # Test that until works
-    sim3 = cv.Sim(pars)
-    sim3.run(until=20)
-    sim3.run(reset_seed=False)
+    s2 = cv.Sim(pars)
+    s2.initialize()
+    for n in range(s2.npts):
+        s2.step()
+    s2.finalize()
 
     # Compare results
     key = 'cum_infections'
-    assert (sim1.results[key][:] == sim2.results[key][:]).all(), 'Next values do not match'
-    assert (sim1.results[key][:] == sim3.results[key][:]).all(), 'Until values do not match'
+    assert (s1.results[key][:] == s2.results[key][:]).all(), 'Next values do not match'
 
-    return sim2
+    return s2
 
 
 #%% Run as a script
@@ -126,7 +122,7 @@ if __name__ == '__main__':
     sim1 = test_resuming()
     sim2 = test_reset_seed()
     sim3 = test_reproducibility()
-    sim4 = test_start_stop()
+    sim4 = test_step()
 
     print('\n'*2)
     sc.toc(T)
