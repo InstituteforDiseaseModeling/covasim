4db2d709e9ac725936b9dcfa6d126fff809e89f9
cliffckerr <questionably@gmail.com>
2020-09-13 17:11:26 -0700

    added regression pars

--- covasim/version.py
+++ covasim/version.py
@@ -2,6 +2,6 @@
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '1.6.0'
-__versiondate__ = '2020-09-07'
+__version__ = '1.6.1'
+__versiondate__ = '2020-09-13'
 __license__ = f'Covasim {__version__} ({__versiondate__}) — © 2020 by IDM'
