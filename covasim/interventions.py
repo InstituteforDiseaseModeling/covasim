fix scaling

covasim/interventions.py
@@ -650,15 +650,15 @@ class test_num(Intervention):
         # Check that there are still tests
         rel_t = t - self.start_day
         if rel_t < len(self.daily_tests):
-            n_tests = int(self.daily_tests[rel_t]/sim['pop_scale'])  # Number of tests for this day -- rescaled
+            n_tests = cvu.randround(self.daily_tests[rel_t]/sim.rescale_vec[t]) # Correct for scaling that may be applied by rounding to the nearest number of tests -- to be replaced with randround()
             if not (n_tests and pl.isfinite(n_tests)): # If there are no tests today, abort early
                 return
             else:
-                sim.results['new_tests'][t] += n_tests*sim['pop_scale']/sim.rescale_vec[t] # Correct for scaling that may be applied
+                sim.results['new_tests'][t] += n_tests
         else:
             return
 
-        test_probs = np.ones(sim.n) # Begin by assigning equal testing probability to everyone
+        test_probs = np.ones(sim.n) # Begin by assigning equal testing weight (converted to a probability) to everyone
 
         # Calculate test probabilities for people with symptoms
         symp_inds = cvu.true(sim.people.symptomatic)
@@ -691,10 +691,18 @@ class test_num(Intervention):
 
         # Don't re-diagnose people
         diag_inds  = cvu.true(sim.people.diagnosed)
-        test_probs[diag_inds] = 0.
+        test_probs[diag_inds] = 0.0
+
+        # With dynamic rescaling, we have to correct for uninfected people outside of the population who would test
+        if sim.rescale_vec[t]/sim['pop_scale'] < 1: # We still have rescaling to do
+            in_pop_tot_prob = test_probs.sum()*sim.rescale_vec[t]
+            out_pop_tot_prob = sim.scaled_pop_size - sim.rescale_vec[t]*sim['pop_size'] # Find out how many people are missing and assign them each weight 1
+            in_frac = in_pop_tot_prob/(in_pop_tot_prob + out_pop_tot_prob) # Fraction of tests which should fall in the sample population
+            n_tests = cvu.randround(n_tests*in_frac) # Recompute the number of tests
 
         # Now choose who gets tested and test them
-        test_inds = cvu.choose_w(probs=test_probs, n=n_tests, unique=False)
+        n_tests = min(n_tests, len(cvu.true(test_probs))) # Don't try to test more people than have nonzero testing probability
+        test_inds = cvu.choose_w(probs=test_probs, n=n_tests, unique=True)
         sim.people.test(test_inds, self.sensitivity, loss_prob=self.loss_prob, test_delay=self.test_delay)
 
         return

covasim/sim.py
@@ -292,7 +292,6 @@ class Sim(cvb.BaseSim):
         # Stock variables
         for key,label in cvd.result_stocks.items():
             self.results[f'n_{key}'] = init_res(label, color=dcols[key])
-        self.results['n_susceptible'].scale = 'static'
 
         # Other variables
         self.results['n_alive']        = init_res('Number of people alive', scale=False)
@@ -582,8 +581,7 @@ class Sim(cvb.BaseSim):
         if self.complete:
             raise AlreadyRunError('Simulation is already complete (call sim.initialize() to re-run)')
 
-        if self.t >= until:
-            # NB. At the start, self.t is None so this check must occur after initialization
+        if self.t >= until: # NB. At the start, self.t is None so this check must occur after initialization
             raise AlreadyRunError(f'Simulation is currently at t={self.t}, requested to run until t={until} which has already been reached')
 
         # Main simulation loop
@@ -632,14 +630,12 @@ class Sim(cvb.BaseSim):
 
         # Scale the results
         for reskey in self.result_keys():
-            if self.results[reskey].scale == 'dynamic':
+            if self.results[reskey].scale in ['dynamic', True]: # "Dynamic" is left for backwards compatibility
                 self.results[reskey].values *= self.rescale_vec
-            elif self.results[reskey].scale == 'static':
-                self.results[reskey].values *= self['pop_scale']
 
         # Calculate cumulative results
         for key in cvd.result_flows.keys():
-            self.results[f'cum_{key}'].values[:] = np.cumsum(self.results[f'new_{key}'].values)
+            self.results[f'cum_{key}'][:] = np.cumsum(self.results[f'new_{key}'][:])
         self.results['cum_infections'].values += self['pop_infected']*self.rescale_vec[0] # Include initially infected people
 
         # Final settings

covasim/utils.py
@@ -247,7 +247,8 @@ def set_seed(seed=None):
 #%% Probabilities -- mostly not jitted since performance gain is minimal
 
 __all__ += ['n_binomial', 'binomial_filter', 'binomial_arr', 'n_multinomial',
-            'poisson', 'n_poisson', 'n_neg_binomial', 'choose', 'choose_r', 'choose_w']
+            'poisson', 'n_poisson', 'n_neg_binomial', 'choose', 'choose_r', 'choose_w',
+            'randround']
 
 def n_binomial(prob, n):
     '''
@@ -426,6 +427,22 @@ def choose_w(probs, n, unique=True):
     else: # Weights are all zero, choose uniformly
         probs = np.ones(n_choices)/n_choices
     return np.random.choice(n_choices, n_samples, p=probs, replace=not(unique))
+
+
+def randround(x):
+    '''
+    Round a float, list, or array probabilistically to the nearest integer.
+
+    To move to Sciris eventually. Based on
+    https://stackoverflow.com/questions/19045971/random-rounding-to-integer-in-python
+    '''
+    if isinstance(x, np.ndarray):
+        output = np.array(np.floor(x+np.random.random(x.size)), dtype=int)
+    elif isinstance (x, list):
+        output = [randround(i) for i in x]
+    else:
+        output = int(np.floor(x+np.random.random()))
+    return output
 
 
 #%% Simple array operations