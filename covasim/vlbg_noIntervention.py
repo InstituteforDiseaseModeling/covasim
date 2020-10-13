89b3cc18262d31e09e8c0a3958a5a3c3a779028a
1-Kelvin <kelvin_sparr@hotmail.de>
2020-10-13 17:21:58 +0200

    changes to vlbg_noIntervention.py

--- covasim/vlbg_noIntervention.py
+++ covasim/vlbg_noIntervention.py
@@ -6,11 +6,11 @@ pars = sc.objdict(
     pop_size     = 40e3,    # Population size
     location = "Vorarlberg",
     pop_infected = 10,       # Number of initial infections
-    n_days       = 40,       # Number of days to simulate
+    n_days       = 90,       # Number of days to simulate
     pop_scale = 10,
     n_beds_icu = 30,
     n_beds_hosp = 80,
-    contacts = 0.5
+    #contacts = 0.5
 )
 
 mysim = cv.sim.Sim(pars=pars,load_pop=True, popfile='voriPop.pop')
@@ -18,5 +18,3 @@ mysim = cv.sim.Sim(pars=pars,load_pop=True, popfile='voriPop.pop')
 mysim.run()
 mysim.plot()
 
-#mysim.reduce()
-mysim.plot()
