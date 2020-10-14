f7b8aaf513bfdd5e2b68df1fafe298f7900555eb
sba5827 <sba5827@students.fhv.at>
2020-10-14 14:14:28 +0200

    updated pars
    Hospital beds and ICUs based on researched data in Vorarlberg.
    ICUs data available at: 2011
    hospital beds data available at: 2019
    
    Signed-off-by: sba5827 <sba5827@students.fhv.at>

--- covasim/vlbg_noIntervention.py
+++ covasim/vlbg_noIntervention.py
@@ -1,22 +1,22 @@
 import covasim as cv
 import sciris as sc
 
+if __name__ == '__main__':
+    pars = sc.objdict(
+        pop_size     = 40e3,    # Population size
+        location = "Vorarlberg",
+        pop_infected = 10,       # Number of initial infections
+        n_days       = 40,       # Number of days to simulate
+        pop_scale = 10,
+        n_beds_icu = 30,
+        n_beds_hosp = 80,
+        contacts = 0.5
+    )
 
-pars = sc.objdict(
-    pop_size     = 40e3,    # Population size
-    location = "Vorarlberg",
-    pop_infected = 10,       # Number of initial infections
-    n_days       = 40,       # Number of days to simulate
-    pop_scale = 10,
-    n_beds_icu = 30,
-    n_beds_hosp = 80,
-    contacts = 0.5
-)
+    mysim = cv.sim.Sim(pars=pars,load_pop=True, popfile='voriPop.pop')
+    #multiSim = cv.MultiSim(sims=mysim,n_runs=2)
+    mysim.run()
+    mysim.plot()
 
-mysim = cv.sim.Sim(pars=pars,load_pop=True, popfile='voriPop.pop')
-#multiSim = cv.MultiSim(sims=mysim,n_runs=2)
-mysim.run()
-mysim.plot()
-
-#mysim.reduce()
-mysim.plot()
+    #mysim.reduce()
+    mysim.plot()
