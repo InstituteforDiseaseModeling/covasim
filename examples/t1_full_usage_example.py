b59709cfc072e38adbaa44849ff04e3860287520
cliffckerr <questionably@gmail.com>
2020-12-05 10:45:45 -0800

    more updates

--- examples/t1_full_usage_example.py
+++ examples/t1_full_usage_example.py
@@ -11,10 +11,10 @@ def protect_elderly(sim):
         sim.people.rel_sus[elderly] = 0.0
 
 pars = dict(
-    pop_size = 50e3,
-    pop_infected = 100,
-    n_days = 90,
-    verbose = 0,
+    pop_size = 50e3, # Have 50,000 people total in the population
+    pop_infected = 100, # Start with 100 infected people
+    n_days = 90, # Run the simulation for 90 days
+    verbose = 0, # Do not print any output
 )
 
 # Running with multisims -- see Tutorial 3
