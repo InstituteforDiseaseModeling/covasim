321d0bfb9d0764e69fac69052fb4dc900f2c0444
cliffckerr <questionably@gmail.com>
2021-01-31 12:56:59 -0800

    update version

--- covasim/version.py
+++ covasim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '2.0.1'
-__versiondate__ = '2020-01-30'
-__license__ = f'Covasim {__version__} ({__versiondate__}) — © 2020 by IDM'
+__versiondate__ = '2020-01-31'
+__license__ = f'Covasim {__version__} ({__versiondate__}) — © 2021 by IDM'
