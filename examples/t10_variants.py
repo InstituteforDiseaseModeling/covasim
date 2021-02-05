287455390997c283b86414ad04b98fa74d99c9b7
Jamie Cohen <jamie.alex.cohen@gmail.com>
2021-02-04 21:17:48 -0500

    updated test script

--- examples/t10_variants.py
+++ examples/t10_variants.py
@@ -1,17 +1,19 @@
 import covasim as cv
 
 
-pars = {'n_strains': 1,
-        'beta': [0.016]}
+pars = {'n_strains': 2,
+        'beta': [0.016, 0.035]}
 
+
+sim = cv.Sim(pars=pars)
+sim.run()
+
+sim.plot_result('cum_infections_by_strain', do_show=True)
+sim.plot_result('incidence_by_strain', label = ['strain1', 'strain2'], do_show=True)
+sim.plot_result('prevalence_by_strain', label = ['strain1', 'strain2'], do_show=True)
+
+## run sim with newly imported strain that's more transmissible on day 10
 imports = cv.import_strain(day=10, n_imports=[5], beta=[0.05], rel_trans=[1], rel_sus=[1])
-# sim = cv.Sim(pars=pars)
-# sim.run()
-#
-# sim.plot_result('cum_infections_by_strain', do_show=True)
-# sim.plot_result('incidence_by_strain', label = ['strain1', 'strain2'], do_show=True)
-# sim.plot_result('prevalence_by_strain', label = ['strain1', 'strain2'], do_show=True)
-
 sim2 = cv.Sim(pars=pars, interventions=imports, label='With imported infections')
 sim2.run()
 
