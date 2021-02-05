dc9e85e39bc7b7f3dd4ec8c898dccd9c0ace91fa
Jamie Cohen <jamie.alex.cohen@gmail.com>
2021-02-04 19:33:03 -0500

    example script for running variants

--- examples/t10_variants.py
+++ examples/t10_variants.py
@@ -1,15 +1,13 @@
 import covasim as cv
 
 
-pars = {'n_strains': 1,
-        'beta': [0.016]}
+pars = {'n_strains': 2,
+        'beta': [0.016, 0.035]}
 sim = cv.Sim(pars=pars)
 sim.run()
 
-sim2 = cv.Sim()
-sim2.run()
-# sim.plot_result('cum_infections_by_strain', do_show=True)
-# sim.plot_result('incidence_by_strain', label = ['strain1', 'strain2'], do_show=True)
-# sim.plot_result('prevalence_by_strain', label = ['strain1', 'strain2'], do_show=True)
+sim.plot_result('cum_infections_by_strain', do_show=True)
+sim.plot_result('incidence_by_strain', label = ['strain1', 'strain2'], do_show=True)
+sim.plot_result('prevalence_by_strain', label = ['strain1', 'strain2'], do_show=True)
 
 print('done')
