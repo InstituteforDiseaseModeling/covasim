fa149bd6996b831a7a0fac18fe90701d32ab23d5
Robyn Stuart <robyna.s@gmail.com>
2021-03-03 12:01:58 +0100

    begin testing - need to investigate speed

--- tests/devtests/test_variants.py
+++ tests/devtests/test_variants.py
@@ -27,25 +27,30 @@ def test_basic_reinfection(do_plot=False, do_show=True, do_save=False):
 
     scenarios = {
         'baseline': {
-          'name':'No reinfection',
-          'pars': {
-              'imm_pars': {k: dict(form='exp_decay', pars={'init_val': 1., 'half_life': None}) for k in cvd.immunity_axes}
-          }
+            'name':'No reinfection',
+            'pars': {
+                'strains': {
+                    'imm_pars': {
+                        k: dict(form='exp_decay', pars={'init_val': 1., 'half_life': None}) for k in cvd.immunity_axes}
+                }
+            }
         },
         'med_halflife': {
-          'name':'3 month waning susceptibility',
-          'pars': {
-              'imm_pars': {k: dict(form='exp_decay', pars={'init_val': 1., 'half_life': 90}) for k in
-                               cvd.immunity_axes}
-          }
+            'name':'3 month waning susceptibility',
+            'pars': {
+                'strains': {
+                    'imm_pars': {k: dict(form='exp_decay', pars={'init_val': 1., 'half_life': 90}) for k in cvd.immunity_axes}
+                }
+            }
         },
         'med_halflife_bysev': {
-          'name':'2 month waning susceptibility for symptomatics only',
-          'pars': {
-              'rel_imm': {'asymptomatic': 0},
-              'imm_pars': {k: dict(form='exp_decay', pars={'init_val': 1., 'half_life': 60}) for k in
-                               cvd.immunity_axes}
-          }
+            'name':'2 month waning susceptibility for symptomatics only',
+            'pars': {
+                'strains': {
+                    'rel_imm': {'asymptomatic': 0},
+                    'imm_pars': {k: dict(form='exp_decay', pars={'init_val': 1., 'half_life': 60}) for k in cvd.immunity_axes}
+                }
+            }
         },
     }
 
@@ -69,13 +74,14 @@ def test_strainduration(do_plot=False, do_show=True, do_save=False):
     sc.heading('Run a sim with 2 strains, one of which has a much longer period before symptoms develop')
     sc.heading('Setting up...')
 
-    imported_strain = {'dur':{'inf2sym': {'dist': 'lognormal_int', 'par1': 10.0, 'par2': 0.9}}}
-    imports = cv.import_strain(strain=imported_strain, days=10, n_imports=30)
+    strain_pars = {'dur':{'inf2sym': {'dist': 'lognormal_int', 'par1': 10.0, 'par2': 0.9}}}
+    strains = cv.Strain(strain=strain_pars, strain_label='10 days til symptoms', days=10, n_imports=30)
     tp = cv.test_prob(symp_prob=0.2) # Add an efficient testing program
 
     base_pars = {
         'beta': 0.015, # Make beta higher than usual so people get infected quickly
         'n_days': 120,
+        'interventions': tp
     }
 
     n_runs = 1
@@ -84,12 +90,12 @@ def test_strainduration(do_plot=False, do_show=True, do_save=False):
     # Define the scenarios
     scenarios = {
         'baseline': {
-          'name':'1 day to symptoms',
-          'pars': {'interventions': [tp]}
+            'name':'1 day to symptoms',
+            'pars': {}
         },
         'slowsymp': {
-          'name':'10 days to symptoms',
-          'pars': {'interventions': [imports, tp]}
+            'name':'10 days to symptoms',
+            'pars': {'strains': strains}
         }
     }
 
@@ -120,7 +126,6 @@ def test_import1strain(do_plot=False, do_show=True, do_save=False):
 
     strain_pars = {
         'rel_beta': 1.5,
-#        'imm_pars': {k: dict(form='exp_decay', pars={'init_val':1., 'half_life':100}) for k in cvd.immunity_axes}
         'imm_pars': {k: dict(form='logistic_decay', pars={'init_val': 1., 'half_val': 10, 'lower_asymp': 0.1, 'decay_rate': -5}) for k in cvd.immunity_axes}
     }
     strain = cv.Strain(strain_pars, days=1)
@@ -137,13 +142,9 @@ def test_import2strains(do_plot=False, do_show=True, do_save=False):
     sc.heading('Test introducing 2 new strains partway through a sim')
     sc.heading('Setting up...')
 
-    strain2 = cv.make_strain('b117')
-    strain3 = cv.make_strain('sa variant')
-
-    imports = [cv.import_strain(strain=strain2, days=10, n_imports=20),
-               cv.import_strain(strain=strain3, days=30, n_imports=20),
-               ]
-    sim = cv.Sim(interventions=imports, label='With imported infections')
+    b117 = cv.Strain('b117', days=10, n_imports=20)
+    p1 = cv.Strain('sa variant', days=30, n_imports=20)
+    sim = cv.Sim(strains=[b117, p1], label='With imported infections')
     sim.run()
 
     strain_labels = [
@@ -172,13 +173,13 @@ def test_importstrain_longerdur(do_plot=False, do_show=True, do_save=False):
         'n_days': 120,
     }
 
-    imported_strain = {
+    strain_pars = {
         'rel_beta': 1.5,
         'dur': {'exp2inf':dict(dist='lognormal_int', par1=6.0,  par2=2.0)}
     }
 
-    imports = cv.import_strain(strain=imported_strain, days=10, n_imports=30)
-    sim = cv.Sim(pars=pars, interventions=imports, label='With imported infections')
+    strain = cv.Strain(strain=strain_pars, strain_label='Custom strain', days=10, n_imports=30)
+    sim = cv.Sim(pars=pars, strains=strain, label='With imported infections')
     sim.run()
 
     if do_plot:
@@ -192,20 +193,16 @@ def test_import2strains_changebeta(do_plot=False, do_show=True, do_save=False):
     sc.heading('Setting up...')
 
     strain2 = {'rel_beta': 1.5,
-               'rel_severe_prob': 1.3,
-               }
+               'rel_severe_prob': 1.3}
 
-    strain3 = {
-        'rel_beta': 2,
-        'rel_symp_prob': 1.6,
-    }
+    strain3 = {'rel_beta': 2,
+               'rel_symp_prob': 1.6}
 
-    intervs  = [
-        cv.change_beta(days=[5, 20, 40], changes=[0.8, 0.7, 0.6]),
-        cv.import_strain(strain=strain2, days=10, n_imports=20),
-        cv.import_strain(strain=strain3, days=30, n_imports=20),
+    intervs  = cv.change_beta(days=[5, 20, 40], changes=[0.8, 0.7, 0.6]),
+    strains  = [cv.Strain(strain=strain2, days=10, n_imports=20),
+                cv.Strain(strain=strain3, days=30, n_imports=20),
                ]
-    sim = cv.Sim(interventions=intervs, label='With imported infections')
+    sim = cv.Sim(interventions=intervs, strains=strains, label='With imported infections')
     sim.run()
 
     strain_labels = [
@@ -282,21 +279,21 @@ if __name__ == '__main__':
     sc.tic()
 
     # Run simplest possible test
-    if 0:
+    if 1:
         sim = cv.Sim()
         sim.run()
 
     # Run more complex tests
     sim1 = test_import1strain(do_plot=do_plot, do_save=do_save, do_show=do_show)
-#    sim2 = test_import2strains(do_plot=do_plot, do_save=do_save, do_show=do_show)
-#    sim3 = test_importstrain_longerdur(do_plot=do_plot, do_save=do_save, do_show=do_show)
+    sim2 = test_import2strains(do_plot=do_plot, do_save=do_save, do_show=do_show)
+    sim3 = test_importstrain_longerdur(do_plot=do_plot, do_save=do_save, do_show=do_show)
 
     # TODO: the next test isn't working, need to check change_beta logic
-    # sim4 = test_import2strains_changebeta(do_plot=do_plot, do_save=do_save, do_show=do_show)
+#    sim4 = test_import2strains_changebeta(do_plot=do_plot, do_save=do_save, do_show=do_show)
 
     # TODO: the next two tests aren't working, need to check scenario logic
-    # scens1 = test_basic_reinfection(do_plot=do_plot, do_save=do_save, do_show=do_show)
-    # scens2 = test_strainduration(do_plot=do_plot, do_save=do_save, do_show=do_show)
+#    scens1 = test_basic_reinfection(do_plot=do_plot, do_save=do_save, do_show=do_show)
+#    scens2 = test_strainduration(do_plot=do_plot, do_save=do_save, do_show=do_show)
 
     # The next tests are deprecated, can be removed
     # simX = test_importstrain_args()
@@ -306,68 +303,4 @@ if __name__ == '__main__':
 
 
 print('Done.')
-
-
-
-# DEPRECATED
-# def test_importstrain_args():
-#     sc.heading('Test flexibility of arguments for the import strain "intervention"')
-#
-#     # Run sim with a single strain initially, then introduce a new strain that's more transmissible on day 10
-#     immunity = [
-#         {'init_immunity': 1., 'half_life': 180, 'cross_factor': 1},
-#     ]
-#     pars = {
-#         'n_strains': 1,
-#         'beta': [0.016],
-#         'immunity': immunity
-#     }
-#
-#     # All these should run
-#     imports = cv.import_strain(days=50, beta=0.03)
-#     #imports = cv.import_strain(days=[10, 50], beta=0.03)
-#     #imports = cv.import_strain(days=50, beta=[0.03, 0.05])
-#     #imports = cv.import_strain(days=[10, 20], beta=[0.03, 0.05])
-#     #imports = cv.import_strain(days=50, beta=[0.03, 0.05, 0.06])
-#     #imports = cv.import_strain(days=[10, 20], n_imports=[5, 10], beta=[0.03, 0.05], init_immunity=[1, 1],
-#     #                          half_life=[180, 180], cross_factor=[0, 0])
-#     #imports = cv.import_strain(days=[10, 50], beta=0.03, cross_factor=[0.4, 0.6])
-#     #imports = cv.import_strain(days=['2020-04-01', '2020-05-01'], beta=0.03)
-#
-#     # This should fail
-#     #imports = cv.import_strain(days=[20, 50], beta=[0.03, 0.05, 0.06])
-#
-#     sim = cv.Sim(pars=pars, interventions=imports)
-#     sim.run()
-#
-#
-#     return sim
-#
-
-# def test_par_refactor():
-#     '''
-#     The purpose of this test is to experiment with different representations of the parameter structures
-#     Still WIP!
-#     '''
-#
-#     # Simplest case: add a strain to beta
-#     p1 = cv.Par(name='beta', val=0.016, by_strain=True)
-#     print(p1.val) # Prints all the stored values of beta
-#     print(p1[0])  # Can index beta like an array to pull out strain-specific values
-#     p1.add_strain(new_val = 0.025)
-#
-#     # Complex case: add a strain that's differentiated by severity for kids 0-20
-#     p2 = cv.Par(name='sus_ORs', val=np.array([0.34, 0.67, 1., 1., 1., 1., 1.24, 1.47, 1.47, 1.47]), by_strain=True, by_age=True)
-#     print(p2.val) # Prints all the stored values for the original strain
-#     print(p2[0])  # Can index beta like an array to pull out strain-specific values
-#     p2.add_strain(new_val=np.array([1., 1., 1., 1., 1., 1., 1.24, 1.47, 1.47, 1.47]))
-#
-#     # Complex case: add a strain that's differentiated by duration of disease
-#     p3 = cv.Par(name='dur_asym2rec', val=dict(dist='lognormal_int', par1=8.0,  par2=2.0), by_strain=True, is_dist=True)
-#     print(p3.val) # Prints all the stored values for the original strain
-#     print(p3[0])  # Can index beta like an array to pull out strain-specific values
-#     p3.add_strain(new_val=dict(dist='lognormal_int', par1=12.0,  par2=2.0))
-#     p3.get(strain=1, n=6)
-#
-#     return p1, p2, p3
 
