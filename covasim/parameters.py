36ab2f308fc9ab18827662685ed5d24f298995a5
Jamie Cohen <jamie.alex.cohen@gmail.com>
2021-04-07 13:56:09 -0400

    fixed init_nab for vaccine boosting

--- covasim/parameters.py
+++ covasim/parameters.py
@@ -67,7 +67,7 @@ def make_pars(set_prognoses=False, prog_by_age=True, version=None, **kwargs):
     pars['NAb_init']                = dict(dist='normal', par1= 0, par2= 2)  # Parameters for the distribution of the initial level of log2(NAb) following natural infection, taken from fig1b of https://doi.org/10.1101/2021.03.09.21252641
     pars['NAb_decay']               = dict(form='nab_decay', pars={'init_decay_rate': np.log(2)/90, 'init_decay_time': 250, 'decay_decay_rate': 0.001}) # Parameters describing the kinetics of decay of NAbs over time, taken from fig3b of https://doi.org/10.1101/2021.03.09.21252641
     pars['NAb_kin']                 = None # Constructed during sim initialization using the NAb_decay parameters
-    pars['NAb_boost']               = 2 # Multiplicative factor applied to a person's NAb levels if they get reinfected. TODO, add source
+    pars['NAb_boost']               = 1.5 # Multiplicative factor applied to a person's NAb levels if they get reinfected. TODO, add source
     pars['NAb_eff']                 = {'sus': {'slope': 2.7, 'n_50': 0.03},
                                        'symp': 0.1, 'sev': 0.52} # Parameters to map NAbs to efficacy
     pars['cross_immunity']          = 0.5   # Default cross-immunity protection factor that applies across different strains
