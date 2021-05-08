8ec4804e6f9cf8c260aa4ec28dc20d83361d4a3c
Katherine Rosenfeld <krosenfeld@idmod.org>
2021-05-08 07:40:19 -0700

    nab kinetics should be length of simulation (n_days + 1)

--- covasim/immunity.py
+++ covasim/immunity.py
@@ -275,7 +275,7 @@ def init_immunity(sim, create=False):
         sim['immunity'] = immunity
 
     # Next, precompute the NAb kinetics and store these for access during the sim
-    sim['nab_kin'] = precompute_waning(length=sim['n_days'], pars=sim['nab_decay'])
+    sim['nab_kin'] = precompute_waning(length=sim['n_days']+1, pars=sim['nab_decay'])
 
     return
 
