7d8b5b05183c1320aaecfad15c3ec47ad2294fb3
Kp <khalid22@my.yorku.ca>
2021-07-31 20:29:36 -0400

    Applying aggregate to plotting.py 02
    Please note this is for a school project with analysing principles of design patterens

--- covasim/plotting.py
+++ covasim/plotting.py
@@ -18,6 +18,71 @@ from . import settings as cvset
 
 __all__ = ['date_formatter', 'plot_sim', 'plot_scens', 'plot_result', 'plot_compare', 'plot_people', 'plotly_sim', 'plotly_people', 'plotly_animate']
 
+def date_formatter(start_day=None, dateformat=None, interval=None, start=None, end=None, ax=None, sim=None):
+    '''
+    Create an automatic date formatter based on a number of days and a start day.
+
+    Wrapper for Matplotlib's date formatter. Note, start_day is not required if the
+    axis uses dates already. To be used in conjunction with setting the x-axis
+    tick label formatter.
+
+    Args:
+        start_day (str/date): the start day, either as a string or date object
+        dateformat (str): the date format (default '%b-%d')
+        interval (int): if supplied, the interval between ticks (must supply an axis also to take effect)
+        start (str/int): if supplied, the lower limit of the axis
+        end (str/int): if supplied, the upper limit of the axis
+        ax (axes): if supplied, automatically set the x-axis formatter for this axis
+        sim (Sim): if supplied, get the start day from this
+
+    **Examples**::
+
+        # Automatically configure the axis with default option
+        cv.date_formatter(sim=sim, ax=ax)
+
+        # Manually configure
+        ax = pl.subplot(111)
+        ax.plot(np.arange(60), np.random.random(60))
+        formatter = cv.date_formatter(start_day='2020-04-04', interval=7, start='2020-05-01', end=50, dateformat='%Y-%m-%d', ax=ax)
+        ax.xaxis.set_major_formatter(formatter)
+    '''
+
+    # Set the default -- "Mar-01"
+    if dateformat is None:
+        dateformat = '%b-%d'
+
+    # Convert to a date object
+    if start_day is None and sim is not None:
+        start_day = sim['start_day']
+    if start_day is None:
+        errormsg = 'If not supplying a start day, you must supply a sim object'
+        raise ValueError(errormsg)
+    start_day = sc.date(start_day)
+
+    @ticker.FuncFormatter
+    def mpl_formatter(x, pos):
+        return (start_day + dt.timedelta(days=int(x))).strftime(dateformat)
+
+    # Set initial tick marks (intervals and limits)
+    if ax is not None:
+
+        # Handle limits
+        xmin, xmax = ax.get_xlim()
+        if start:
+            xmin = sc.day(start, start_day=start_day)
+        if end:
+            xmax = sc.day(end, start_day=start_day)
+        ax.set_xlim((xmin, xmax))
+
+        # Set the x-axis intervals
+        if interval:
+            ax.set_xticks(np.arange(xmin, xmax+1, interval))
+
+        # Set the formatter
+        ax.xaxis.set_major_formatter(mpl_formatter)
+
+    return mpl_formatter
+
 
 #%% Plotting helper functions
 
@@ -37,6 +102,28 @@ def handle_args(fig_args=None, plot_args=None, scatter_args=None, axis_args=None
     defaults.date    = sc.objdict(as_dates=True, dateformat=None, interval=None, rotation=None, start_day=None, end_day=None)
     defaults.show    = sc.objdict(data=True, ticks=True, interventions=True, legend=True)
     defaults.mpl     = sc.objdict(dpi=None, fontsize=None, fontfamily=None) # Use Covasim global defaults
+
+
+def create_figs(args, sep_figs, fig=None, ax=None):
+    '''
+    Create the figures and set overall figure properties. If a figure is supplied,
+    reset the axes labels for automatic use by other plotting functions (i.e. ax1, ax2, etc.)
+    '''
+    if sep_figs:
+        fig = None
+        figs = []
+    else:
+        if fig is None:
+            if ax is None:
+                fig = pl.figure(**args.fig) # Create the figure if none is supplied
+            else:
+                fig = ax.figure
+        else:
+            for i,fax in enumerate(fig.axes):
+                fax.set_label(f'ax{i+1}')
+        figs = None
+    pl.subplots_adjust(**args.axis)
+    return fig, figs
 
     # Handle directly supplied kwargs
     for dkey,default in defaults.items():
@@ -116,27 +203,6 @@ def handle_to_plot(kind, to_plot, n_cols, sim, check_ready=True):
 
     return to_plot, n_cols, n_rows
 
-
-def create_figs(args, sep_figs, fig=None, ax=None):
-    '''
-    Create the figures and set overall figure properties. If a figure is supplied,
-    reset the axes labels for automatic use by other plotting functions (i.e. ax1, ax2, etc.)
-    '''
-    if sep_figs:
-        fig = None
-        figs = []
-    else:
-        if fig is None:
-            if ax is None:
-                fig = pl.figure(**args.fig) # Create the figure if none is supplied
-            else:
-                fig = ax.figure
-        else:
-            for i,fax in enumerate(fig.axes):
-                fax.set_label(f'ax{i+1}')
-        figs = None
-    pl.subplots_adjust(**args.axis)
-    return fig, figs
 
 
 def create_subplots(figs, fig, shareax, n_rows, n_cols, pnum, fig_args, sep_figs, log_scale, title):
@@ -222,72 +288,6 @@ def title_grid_legend(ax, title, grid, commaticks, setylim, legend_args, show_le
             sc.commaticks(ax=ax)
 
     return
-
-
-def date_formatter(start_day=None, dateformat=None, interval=None, start=None, end=None, ax=None, sim=None):
-    '''
-    Create an automatic date formatter based on a number of days and a start day.
-
-    Wrapper for Matplotlib's date formatter. Note, start_day is not required if the
-    axis uses dates already. To be used in conjunction with setting the x-axis
-    tick label formatter.
-
-    Args:
-        start_day (str/date): the start day, either as a string or date object
-        dateformat (str): the date format (default '%b-%d')
-        interval (int): if supplied, the interval between ticks (must supply an axis also to take effect)
-        start (str/int): if supplied, the lower limit of the axis
-        end (str/int): if supplied, the upper limit of the axis
-        ax (axes): if supplied, automatically set the x-axis formatter for this axis
-        sim (Sim): if supplied, get the start day from this
-
-    **Examples**::
-
-        # Automatically configure the axis with default option
-        cv.date_formatter(sim=sim, ax=ax)
-
-        # Manually configure
-        ax = pl.subplot(111)
-        ax.plot(np.arange(60), np.random.random(60))
-        formatter = cv.date_formatter(start_day='2020-04-04', interval=7, start='2020-05-01', end=50, dateformat='%Y-%m-%d', ax=ax)
-        ax.xaxis.set_major_formatter(formatter)
-    '''
-
-    # Set the default -- "Mar-01"
-    if dateformat is None:
-        dateformat = '%b-%d'
-
-    # Convert to a date object
-    if start_day is None and sim is not None:
-        start_day = sim['start_day']
-    if start_day is None:
-        errormsg = 'If not supplying a start day, you must supply a sim object'
-        raise ValueError(errormsg)
-    start_day = sc.date(start_day)
-
-    @ticker.FuncFormatter
-    def mpl_formatter(x, pos):
-        return (start_day + dt.timedelta(days=int(x))).strftime(dateformat)
-
-    # Set initial tick marks (intervals and limits)
-    if ax is not None:
-
-        # Handle limits
-        xmin, xmax = ax.get_xlim()
-        if start:
-            xmin = sc.day(start, start_day=start_day)
-        if end:
-            xmax = sc.day(end, start_day=start_day)
-        ax.set_xlim((xmin, xmax))
-
-        # Set the x-axis intervals
-        if interval:
-            ax.set_xticks(np.arange(xmin, xmax+1, interval))
-
-        # Set the formatter
-        ax.xaxis.set_major_formatter(mpl_formatter)
-
-    return mpl_formatter
 
 
 def reset_ticks(ax, sim=None, date_args=None, start_day=None):
