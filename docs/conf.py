8262e63c585625b7082143bdd962f772fa04b7a1
Jen Schripsema <jen.schripsema@gatesfoundation.org>
2023-08-01 11:40:16 -0600

    deleted README.md entirely

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ release = cv.__version__
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # suppress warnings for multiple possible Python references in the namespace
 # suppress_warnings = ['ref.python']
