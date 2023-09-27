216c2981a05a966cb67fafbc864ef96c8c3b6968
Jen Schripsema <jen.schripsema@gatesfoundation.org>
2023-09-27 16:44:10 -0600

    updated docs/requirements.txt with most recent packages, updated CSS, https://github.com/InstituteforDiseaseModeling/idm-content/issues/58

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,10 @@ html_favicon = "images/favicon.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ['_static']
+
+html_css_files = ['theme_overrides.css']
 
 html_context = {
     'rtd_url': 'https://docs.idmod.org/projects/covasim/en/latest',
