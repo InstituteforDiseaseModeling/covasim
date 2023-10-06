9fcffedd04237bef1dae2c000fda8b55136ab046
Jen Schripsema <jschripsema@idmod.org>
2023-10-06 17:12:40 -0600

    Update conf.py
    Added Markdown to source file types for docs/conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
